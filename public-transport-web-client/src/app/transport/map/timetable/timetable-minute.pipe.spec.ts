97bd3518f7be650ed154db08c1b75cb2f020fd3d
przemekwrona <przemekwrona25@gmail.com>
2024-07-17 11:35:50 +0200

    feat: add timetables component

---
+++ public-transport-web-client/src/app/transport/map/timetable/timetable-minute.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { TimetableMinutePipe } from './timetable-minute.pipe';
+
+describe('TimetableMinutePipe', () => {
+  it('create an instance', () => {
+    const pipe = new TimetableMinutePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
