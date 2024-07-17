8c875a02106f15af732bf1ec7e3e07ee63368247
przemekwrona <przemekwrona25@gmail.com>
2024-07-17 11:35:04 +0200

    feat: adapt departure pipe

--- public-transport-web-client/src/app/transport/map/departures/departure-time-prefix.pipe.ts
+++ public-transport-web-client/src/app/transport/map/departures/departure-time-prefix.pipe.ts
@@ -3,21 +3,21 @@ import {map, Observable, timer} from "rxjs";
 import moment from "moment";
 
 @Pipe({
-  name: 'departureTimePrefix'
+    name: 'departureTimePrefix'
 })
 export class DepartureTimePrefixPipe implements PipeTransform {
 
-  transform(value: Date): Observable<string> {
-    return timer(0, 1000)
-      .pipe(map(() => {
-        const now = moment(value);
-        const differenceInMinutes = now.diff(moment(), 'minutes');
+    transform(value: number | undefined): Observable<string> {
+        return timer(0, 1000)
+            .pipe(map(() => {
+                const now = moment().startOf('day').add(value, 'second');
+                const differenceInMinutes = now.diff(moment(), 'minutes');
 
-        if (differenceInMinutes <= 60) {
-          return 'za'
-        }
-        return '';
-      }));
-  }
+                if (differenceInMinutes <= 60) {
+                    return 'za'
+                }
+                return '';
+            }));
+    }
 
 }
