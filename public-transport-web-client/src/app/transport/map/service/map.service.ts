9f3c4a4f2e4ac0e4d7be29cadd46e4ba924f0517
przemekwrona <przemekwrona25@gmail.com>
2024-08-21 08:57:17 +0200

    feat: init map service

---
+++ public-transport-web-client/src/app/transport/map/service/map.service.ts
@@ -0,0 +1,46 @@
+import {Injectable} from '@angular/core';
+import * as L from "leaflet";
+import {Map} from "leaflet";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class MapService {
+
+    private ZOOM: number = 16;
+    private map: Map;
+
+    constructor() {
+    }
+
+    public initMap(city: string): Map {
+        this.map = L.map('map', {
+            center: [52.23210, 21.00585],
+            zoom: this.ZOOM,
+            zoomControl: false
+        });
+
+        L.control.zoom({
+            position: 'bottomright'
+        }).addTo(this.map);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 18,
+            minZoom: 3,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(this.map);
+
+        return this.map;
+    }
+
+    public getMap(): Map {
+        return this.map;
+    }
+
+    public getZoom(): number {
+        if (this.map == null) {
+            return 15;
+        }
+        return this.map.getZoom();
+    }
+}
