dc7839d3771ba09ebf8017966b83ae5dad4f4bc6
przemekwrona <przemekwrona25@gmail.com>
2025-01-03 19:18:07 +0100

    feat: request login method

--- public-transport-web-client/src/app/auth/auth.module.ts
+++ public-transport-web-client/src/app/auth/auth.module.ts
@@ -1,15 +1,17 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {SigninComponent} from "./signin/signin.component";
-
+import {FormsModule} from "@angular/forms";
 
 
 @NgModule({
-  declarations: [
-    SigninComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+    declarations: [
+        SigninComponent
+    ],
+    imports: [
+        CommonModule,
+        FormsModule
+    ]
 })
-export class AuthModule { }
+export class AuthModule {
+}
