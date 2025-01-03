512d681228f9d25249860f61ba683b810ff49237
przemekwrona <przemekwrona25@gmail.com>
2025-01-03 12:32:24 +0100

    feat: change id of test data

--- public-transport-web-server/src/test/resources/sql/init.sql
+++ public-transport-web-server/src/test/resources/sql/init.sql
@@ -1,5 +1,5 @@
-INSERT INTO agency(agency_id, agency_code, agency_name) VALUES (1, 'NEXTBUS', 'NextBus S.A.');
-INSERT INTO agency(agency_id, agency_code, agency_name) VALUES (2, 'BESKIDBUS', 'Beskidzka Komunikacja Miejska');
+INSERT INTO agency(agency_id, agency_code, agency_name) VALUES (100, 'NEXTBUS', 'NextBus S.A.');
+INSERT INTO agency(agency_id, agency_code, agency_name) VALUES (101, 'BESKIDBUS', 'Beskidzka Komunikacja Miejska');
 
 INSERT INTO bdot10k_stop(bdot10k_id, name, lon, lat) VALUES ('10032', 'Morawica', 21.0507955, 52.1546729);
 INSERT INTO bdot10k_stop(bdot10k_id, name, lon, lat) VALUES ('10033', 'Chmielnik', 23.6511807, 51.2666132);
