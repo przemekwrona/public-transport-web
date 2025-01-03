8a9874811d7607a99f3631f7fe47aa53882a58da
przemekwrona <przemekwrona25@gmail.com>
2025-01-03 19:50:42 +0100

    feat: add Authorization token

--- public-transport-web-client/src/app/app.module.ts
+++ public-transport-web-client/src/app/app.module.ts
@@ -5,12 +5,13 @@ import {BrowserModule} from "@angular/platform-browser";
 import {MapModule} from "./transport/map/map.module";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {TransportModule} from "./transport/transport.module";
-import {HttpClientModule} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {FormsModule} from "@angular/forms";
 import {RouterModule} from "@angular/router";
 import {routes} from "./app.routes";
 import {AgencyModule} from "./agency/agency.module";
 import {AuthModule} from "./auth/auth.module";
+import {AddHeaderInterceptor} from "./auth/auth.interceptor";
 
 
 @NgModule({
@@ -32,6 +33,11 @@ import {AuthModule} from "./auth/auth.module";
     declarations: [
         AppComponent
     ],
+    providers: [{
+        provide: HTTP_INTERCEPTORS,
+        useClass: AddHeaderInterceptor,
+        multi: true,
+    }],
     bootstrap: [
         AppComponent
     ]
