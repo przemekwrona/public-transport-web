8211d0342078eb55bd0770ee26a8e2938951d411
przemekwrona <przemekwrona25@gmail.com>
2025-03-11 13:46:37 +0100

    feat: animate show and hide component

--- public-transport-web-client/src/app/agency/shared/calendar-input/calendar-input.component.ts
+++ public-transport-web-client/src/app/agency/shared/calendar-input/calendar-input.component.ts
@@ -1,11 +1,23 @@
 import {Component, forwardRef} from '@angular/core';
 import moment from "moment";
 import {ControlValueAccessor, NG_VALUE_ACCESSOR} from "@angular/forms";
+import {animate, style, transition, trigger} from "@angular/animations";
 
 @Component({
     selector: 'app-calendar-input',
     templateUrl: './calendar-input.component.html',
     styleUrl: './calendar-input.component.scss',
+    animations: [
+        trigger('fadeInOut', [
+            transition(':enter', [
+                style({ opacity: 0 }),
+                animate('500ms ease-in', style({ opacity: 1 }))
+            ]),
+            transition(':leave', [
+                animate('500ms ease-out', style({ opacity: 0 }))
+            ])
+        ])
+    ],
     providers: [
         {
             provide: NG_VALUE_ACCESSOR,
