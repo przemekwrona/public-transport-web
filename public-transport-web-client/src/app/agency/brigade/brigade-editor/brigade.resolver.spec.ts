e4df629d0e81eec584728c8bf6b0732e88154895
przemekwrona <przemekwrona25@gmail.com>
2025-03-30 13:33:54 +0200

    feat: save brigade

---
+++ public-transport-web-client/src/app/agency/brigade/brigade-editor/brigade.resolver.spec.ts
@@ -0,0 +1,19 @@
+import { TestBed } from '@angular/core/testing';
+import { ResolveFn } from '@angular/router';
+
+import { brigadeResolver } from './brigade.resolver';
+import {Observable} from "rxjs";
+import {BrigadeBody} from "../../../generated/public-transport";
+
+describe('brigadeResolver', () => {
+  const executeResolver: ResolveFn<Observable<BrigadeBody>> = (...resolverParameters) =>
+      TestBed.runInInjectionContext(() => brigadeResolver(...resolverParameters));
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+  });
+
+  it('should be created', () => {
+    expect(executeResolver).toBeTruthy();
+  });
+});
