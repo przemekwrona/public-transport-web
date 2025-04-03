b0ae21a2485c8234bc61a4264e2a782f169b9efc
przemekwrona <przemekwrona25@gmail.com>
2025-04-03 12:55:40 +0200

    feat: get saved calendars

--- public-transport-web-server/src/main/java/pl/wrona/webserver/agency/calendar/CalendarController.java
+++ public-transport-web-server/src/main/java/pl/wrona/webserver/agency/calendar/CalendarController.java
@@ -3,6 +3,7 @@ package pl.wrona.webserver.agency.calendar;
 import lombok.AllArgsConstructor;
 import org.igeolab.iot.pt.server.api.CalendarApi;
 import org.igeolab.iot.pt.server.api.model.CalendarPayload;
+import org.igeolab.iot.pt.server.api.model.GetCalendarsResponse;
 import org.igeolab.iot.pt.server.api.model.Status;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,4 +19,10 @@ public class CalendarController implements CalendarApi {
     public ResponseEntity<Status> createCalendar(CalendarPayload calendarPayload) {
         return ResponseEntity.status(HttpStatus.CREATED).body(calendarService.createCalendar(calendarPayload));
     }
+
+    @Override
+    public ResponseEntity<GetCalendarsResponse> getCalendars() {
+        return ResponseEntity.ok(calendarService.getCalendars());
+    }
+
 }
