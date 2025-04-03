6a88788b80565138178bb99b1f63f9d4502e47aa
przemekwrona <przemekwrona25@gmail.com>
2025-04-03 13:50:30 +0200

    feat: return excluded and included dates

--- public-transport-web-server/src/main/java/pl/wrona/webserver/agency/calendar/CalendarDatesRepository.java
+++ public-transport-web-server/src/main/java/pl/wrona/webserver/agency/calendar/CalendarDatesRepository.java
@@ -1,8 +1,17 @@
 package pl.wrona.webserver.agency.calendar;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import pl.wrona.webserver.agency.entity.Agency;
+
+import java.util.List;
 
 @Repository
 public interface CalendarDatesRepository extends JpaRepository<CalendarDatesEntity, CalendarDatesId> {
+
+    @Query("SELECT cd FROM CalendarDatesEntity cd WHERE cd.calendar.agency = :agency")
+    List<CalendarDatesEntity> findAllByAgency(@Param("agency") Agency agency);
+
 }
