96e6b9624d1630a2be6a04cf9a2c17ea9134b657
przemekwrona <przemekwrona25@gmail.com>
2025-04-03 12:15:24 +0200

    feat: call save calendars in typescript

--- public-transport-web-client/src/app/agency/calendars/calendars.service.ts
+++ public-transport-web-client/src/app/agency/calendars/calendars.service.ts
@@ -1,11 +1,17 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {CalendarPayload, Status} from "../../generated/public-transport";
+import {Observable} from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class CalendarsService {
 
-  constructor(private httpClient: HttpClient) {
-  }
+    constructor(private httpClient: HttpClient) {
+    }
+
+    public createCalendar(payload: CalendarPayload): Observable<Status> {
+        return this.httpClient.post<Status>(`/api/v1/calendar`, payload);
+    }
 }
