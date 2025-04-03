f6de4f1d5e4d7eb9df75f88f929da4c4a3fde4c5
przemekwrona <przemekwrona25@gmail.com>
2025-04-03 13:17:25 +0200

    feat: define calendars in component calendars.component.ts

--- public-transport-web-client/src/app/agency/calendars/calendars/calendars.component.ts
+++ public-transport-web-client/src/app/agency/calendars/calendars/calendars.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {GetCalendarsResponse} from "../../../generated/public-transport";
 
 @Component({
-  selector: 'app-calendars',
-  templateUrl: './calendars.component.html',
-  styleUrl: './calendars.component.scss'
+    selector: 'app-calendars',
+    templateUrl: './calendars.component.html',
+    styleUrl: './calendars.component.scss'
 })
-export class CalendarsComponent {
+export class CalendarsComponent implements OnInit {
+
+    public calendarsResponse: GetCalendarsResponse;
+
+    constructor(private route: ActivatedRoute) {
+    }
+
+    ngOnInit(): void {
+        this.calendarsResponse = this.route.snapshot.data['calendars'];
+    }
 
 }
