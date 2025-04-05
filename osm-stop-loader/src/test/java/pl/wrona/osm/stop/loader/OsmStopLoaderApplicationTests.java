0bc4e90ab14ccb59a536387f41066c2eca85eccc
przemekwrona <przemekwrona25@gmail.com>
2025-04-05 17:28:27 +0200

    feat: init load stops module

---
+++ osm-stop-loader/src/test/java/pl/wrona/osm/stop/loader/OsmStopLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wrona.osm.stop.loader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsmStopLoaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
