c336e569fe0281e17a420b598964c20f22af921e
przemekwrona <przemekwrona25@gmail.com>
2025-04-05 17:28:27 +0200

    feat: init load stops module

---
+++ osm-stop-loader/src/main/java/pl/wrona/osm/stop/loader/OsmStopLoaderApplication.java
@@ -0,0 +1,13 @@
+package pl.wrona.osm.stop.loader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsmStopLoaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OsmStopLoaderApplication.class, args);
+    }
+
+}
---
+++ osm-stop-loader/src/main/java/pl/wrona/osm/stop/loader/OsmStopLoaderJob.java
@@ -0,0 +1,14 @@
+package pl.wrona.osm.stop.loader;
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OsmStopLoaderJob implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
