61a2e784621efb58f0af80d6d44aad7eb93ddceb
przemekwrona <przemekwrona25@gmail.com>
2025-04-06 14:38:23 +0200

    feat: save OSM stops

--- osm-stop-loader/src/main/java/pl/wrona/osm/stop/loader/StopResource.java
+++ osm-stop-loader/src/main/java/pl/wrona/osm/stop/loader/StopResource.java
@@ -1,11 +1,14 @@
 package pl.wrona.osm.stop.loader;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.openstreetmap.osmosis.core.domain.v0_6.Node;
 import org.openstreetmap.osmosis.core.domain.v0_6.Tag;
+import org.springframework.jdbc.BadSqlGrammarException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+@Slf4j
 @Repository
 @AllArgsConstructor
 public class StopResource {
@@ -19,7 +22,12 @@ public class StopResource {
 
     public void save(String bdot10kId, String name, double lat, double lon) {
         String query = INSERT_QUERY.formatted(bdot10kId, name, lat, lon);
-        jdbcTemplate.update(query);
+        try {
+            jdbcTemplate.update(query);
+        } catch (BadSqlGrammarException ex) {
+            log.error("Wrong SQL syntax {}", query);
+        }
+
     }
 
     public void save(Node node) {
