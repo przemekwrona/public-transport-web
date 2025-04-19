b6e72d9ad4ea146e6fd41c681c1322722c366b5c
przemekwrona <przemekwrona25@gmail.com>
2025-04-19 21:11:57 +0200

    feat: add text to landing page

--- public-transport-web-client/src/app/landing/landing.component.ts
+++ public-transport-web-client/src/app/landing/landing.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
-  selector: 'app-landing',
-  templateUrl: './landing.component.html',
-  styleUrl: './landing.component.css'
+    selector: 'app-landing',
+    templateUrl: './landing.component.html',
+    styleUrl: './landing.component.scss'
 })
 export class LandingComponent {
+
+    constructor(private _router: Router) {
+    }
+
+    public scrollToProduct(): void {
+        this.scrollToElement("#product");
+    }
+
+    public scrollToAbout(): void {
+        this.scrollToElement("#about");
+    }
+
+    public scrollToElement(elementId: string): void {
+        const element = document.querySelector(elementId);
+        if (element) element.scrollIntoView({behavior: 'smooth', block: 'start'});
+    }
+
+    public navigateToLogin(): void {
+        this._router.navigate(['/signin']).then(() => {
+        });
+    }
 
 }
