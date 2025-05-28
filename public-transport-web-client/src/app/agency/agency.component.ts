4c47760503ccf73f346518a64f0dae3cc73a314c
przemekwrona <przemekwrona25@gmail.com>
2025-05-28 10:11:25 +0200

    feat: drop GET routes in agency.component.ts

--- public-transport-web-client/src/app/agency/agency.component.ts
+++ public-transport-web-client/src/app/agency/agency.component.ts
@@ -1,7 +1,5 @@
 import {AfterViewInit, Component, HostBinding, OnInit} from '@angular/core';
-import {RoutesService} from "./routes/routes.service";
-import {Route, Routes} from "../generated/public-transport";
-import {Router} from "@angular/router";
+import {Routes} from "../generated/public-transport";
 import KTComponents from "../../metronic/core";
 import KTLayout from '../../metronic/app/layouts/demo1';
 
@@ -24,17 +22,7 @@ export class AgencyComponent implements OnInit, AfterViewInit {
   ngOnInit(): void {
   }
 
-  constructor(private routeService: RoutesService, private _router: Router) {
-    this.routeService.getRoutes().subscribe((routes: Routes) => {
-      this.routes = routes;
-    })
-  }
-
-  public openTrip(route: Route): void {
-    const state = {name: route.name, line: route.line};
-    console.log(state);
-    this._router.navigate(['/agency/trips'], {state: state});
-    // this._router.navigateByUrl('/agency/trips', {state: state});
+  constructor() {
   }
 
 }
