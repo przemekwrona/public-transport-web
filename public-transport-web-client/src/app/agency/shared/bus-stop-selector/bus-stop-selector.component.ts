a705500dbbfbad892ef94ac1a6bd0c263221f71e
przemekwrona <przemekwrona25@gmail.com>
2025-05-28 10:18:51 +0200

    feat: select bus stop from selector

--- public-transport-web-client/src/app/agency/shared/bus-stop-selector/bus-stop-selector.component.ts
+++ public-transport-web-client/src/app/agency/shared/bus-stop-selector/bus-stop-selector.component.ts
@@ -37,12 +37,12 @@ export class BusStopSelectorComponent {
     /**
      * Holds the current value of the slider
      */
-    @Input() busStopId: string = "";
+    @Input() busStopId: BusStopSelectorData = {} as BusStopSelectorData;
 
     /**
      * Invoked when the model has been changed
      */
-    @Output() busStopIdChange: EventEmitter<string> = new EventEmitter<string>();
+    @Output() busStopIdChange: EventEmitter<BusStopSelectorData> = new EventEmitter<BusStopSelectorData>();
 
 
     @ViewChild('toggleInput') toggleButton: ElementRef;
@@ -51,34 +51,42 @@ export class BusStopSelectorComponent {
     busStops: Stop[] = [];
     showOptions = false;
     stopNamePrefix = '';
+    lastSearchCity: string = '';
 
     constructor(private renderer: Renderer2, private stopsService: StopsService) {
         /**
          * This events get called by all clicks on the page
          */
         // this.renderer.listen('window', 'click', (e: Event) => {
-            /**
-             * Only run when toggleButton is not clicked
-             * If we don't check this, all clicks (even on the toggle button) gets into this
-             * section which in the result we might never see the menu open!
-             * And the menu itself is checked here, and it's where we check just outside of
-             * the menu and button the condition abbove must close the menu
-             */
-            // if (e.target !== this.toggleButton.nativeElement && e.target !== this.menu.nativeElement) {
-            //     this.showOptions = false;
-            // }
+        /**
+         * Only run when toggleButton is not clicked
+         * If we don't check this, all clicks (even on the toggle button) gets into this
+         * section which in the result we might never see the menu open!
+         * And the menu itself is checked here, and it's where we check just outside of
+         * the menu and button the condition abbove must close the menu
+         */
+        // if (e.target !== this.toggleButton.nativeElement && e.target !== this.menu.nativeElement) {
+        //     this.showOptions = false;
+        // }
         // });
     }
 
     onSelect(option: Stop) {
         this.stopNamePrefix = option.name;
         this.showOptions = false;
-        this.busStopIdChange.emit(option.id);
+
+        const busStopSelectorData: BusStopSelectorData = {} as BusStopSelectorData;
+        busStopSelectorData.stopId = option.id;
+        busStopSelectorData.stopName = option.name;
+        busStopSelectorData.stopLon = option.lon;
+        busStopSelectorData.stopLat = option.lat;
+
+        this.busStopIdChange.emit(busStopSelectorData);
     }
 
-    onWrite(stopNamePrefix: string): void {
+    onWrite(): void {
         if (this.stopNamePrefix.length > 2) {
-            this.stopsService.findStopsByStopName(stopNamePrefix).subscribe((response: StopsResponse) => {
+            this.stopsService.findStopsByStopName(this.stopNamePrefix).subscribe((response: StopsResponse) => {
                 this.busStops = response.stops
                 this.showOptions = true;
             });
@@ -89,18 +97,13 @@ export class BusStopSelectorComponent {
         const dialogRef = this.dialog.open(BusStopModalSelectorComponent, {
             width: '90%',
             height: '70%',
-            data: {
-                stopId: this.busStopId,
-                stopName: this.busStopId,
-                stopLon: 52.00,
-                stopLat: 21.00
-            },
+            data: this.busStopId,
         });
 
-        dialogRef.afterClosed().subscribe(result => {
-            if (result !== undefined) {
-                console.log(result);
-                // 'this.animal.set(result);'
+        dialogRef.afterClosed().subscribe((busStopSelectorData: BusStopSelectorData | undefined) => {
+            if (busStopSelectorData !== undefined) {
+                this.stopNamePrefix = busStopSelectorData.stopName;
+                this.busStopIdChange.emit(busStopSelectorData);
             }
         });
     }
