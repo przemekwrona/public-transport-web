df9e5a768a76f3ce1942199f6dea6883a570298a
przemekwrona <przemekwrona25@gmail.com>
2025-05-28 11:21:47 +0200

    feat: improve select city

--- public-transport-web-client/src/app/agency/shared/bus-stop-selector/bus-stop-selector.component.ts
+++ public-transport-web-client/src/app/agency/shared/bus-stop-selector/bus-stop-selector.component.ts
@@ -25,8 +25,7 @@ export interface BusStopSelectorData {
 @Component({
     selector: 'app-bus-stop-selector',
     templateUrl: './bus-stop-selector.component.html',
-    styleUrl: './bus-stop-selector.component.scss',
-    changeDetection: ChangeDetectionStrategy.OnPush
+    styleUrl: './bus-stop-selector.component.scss'
 })
 export class BusStopSelectorComponent {
     readonly dialog = inject(MatDialog);
@@ -51,41 +50,24 @@ export class BusStopSelectorComponent {
     busStops: Stop[] = [];
     showOptions = false;
     stopNamePrefix = '';
-    lastSearchCity: string = '';
 
     constructor(private renderer: Renderer2, private stopsService: StopsService) {
-        /**
-         * This events get called by all clicks on the page
-         */
-        // this.renderer.listen('window', 'click', (e: Event) => {
-        /**
-         * Only run when toggleButton is not clicked
-         * If we don't check this, all clicks (even on the toggle button) gets into this
-         * section which in the result we might never see the menu open!
-         * And the menu itself is checked here, and it's where we check just outside of
-         * the menu and button the condition abbove must close the menu
-         */
-        // if (e.target !== this.toggleButton.nativeElement && e.target !== this.menu.nativeElement) {
-        //     this.showOptions = false;
-        // }
-        // });
     }
 
     onSelect(option: Stop) {
-        this.stopNamePrefix = option.name;
         this.showOptions = false;
+        this.stopNamePrefix = option.name;
 
         const busStopSelectorData: BusStopSelectorData = {} as BusStopSelectorData;
         busStopSelectorData.stopId = option.id;
         busStopSelectorData.stopName = option.name;
         busStopSelectorData.stopLon = option.lon;
         busStopSelectorData.stopLat = option.lat;
-
         this.busStopIdChange.emit(busStopSelectorData);
     }
 
     onWrite(): void {
-        if (this.stopNamePrefix.length > 2) {
+        if (this.stopNamePrefix.length >= 3) {
             this.stopsService.findStopsByStopName(this.stopNamePrefix).subscribe((response: StopsResponse) => {
                 this.busStops = response.stops
                 this.showOptions = true;
