491983eccde62d6ad08be06563310ef9333f268d
przemekwrona <przemekwrona25@gmail.com>
2025-06-03 11:07:19 +0200

    fix: create route

--- public-transport-web-client/src/app/agency/routes/create-route/create-route.component.ts
+++ public-transport-web-client/src/app/agency/routes/create-route/create-route.component.ts
@@ -1,6 +1,6 @@
 import {Component, ElementRef, Renderer2, ViewChild} from '@angular/core';
 import {RoutesService} from "../routes.service";
-import {Route, Status} from "../../../generated/public-transport";
+import {Route, Status, Stop} from "../../../generated/public-transport";
 import {Router} from "@angular/router";
 import {BusStopSelectorData} from "../../shared/bus-stop-selector/bus-stop-selector.component";
 
@@ -23,6 +23,17 @@ export class CreateRouteComponent {
     }
 
     public createRoute(): void {
+        const originStop: Stop = {} as Stop;
+        originStop.id = this.origin.stopId;
+        originStop.name = this.origin.stopName
+
+        const destinationStop: Stop = {} as Stop;
+        destinationStop.id = this.destination.stopId;
+        destinationStop.name = this.destination.stopName;
+
+        this.route.originStop = originStop;
+        this.route.destinationStop = destinationStop;
+
         this.routeService.createRoute(this.route).subscribe((status: Status) => {
             this._router.navigate(['/agency/trips/create'], {
                 queryParams: {
