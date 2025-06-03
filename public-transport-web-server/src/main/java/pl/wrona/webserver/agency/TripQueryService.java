44320572ec99c1a6ac2b6a0706f584752bd961db
przemekwrona <przemekwrona25@gmail.com>
2025-06-03 12:06:33 +0200

    feat: fix create brigade

--- public-transport-web-server/src/main/java/pl/wrona/webserver/agency/TripQueryService.java
+++ public-transport-web-server/src/main/java/pl/wrona/webserver/agency/TripQueryService.java
@@ -6,6 +6,7 @@ import org.igeolab.iot.pt.server.api.model.Trip;
 import org.igeolab.iot.pt.server.api.model.Trips;
 import org.springframework.stereotype.Service;
 import pl.wrona.webserver.agency.entity.RouteEntity;
+import pl.wrona.webserver.agency.entity.StopEntity;
 import pl.wrona.webserver.agency.entity.TripEntity;
 import pl.wrona.webserver.agency.mapper.RouteMapper;
 import pl.wrona.webserver.agency.mapper.TripMapper;
@@ -21,14 +22,23 @@ import java.util.stream.Collectors;
 public class TripQueryService {
 
     private TripRepository tripRepository;
+    private StopService stopService;
 
     public GetAllTripsResponse getTripsByLineOrName(String lineOrName) {
         Map<RouteEntity, Set<TripEntity>> tripSet = tripRepository.findByLineOrNameContainingIgnoreCase(lineOrName).stream()
                 .collect(Collectors.groupingBy(TripEntity::getRoute, Collectors.toSet()));
 
+
+        List<String> stopsIds = tripSet.keySet().stream()
+                .map(routeEntity -> List.of(routeEntity.getOriginStopId(), routeEntity.getDestinationStopId()))
+                .flatMap(List::stream)
+                .toList();
+
+        Map<String, StopEntity> stopDictionary = this.stopService.mapStopByIdsIn(stopsIds);
+
         List<Trips> tripsResponse = tripSet.keySet().stream()
                 .map(route -> new Trips()
-                        .route(RouteMapper.map(route, Map.of()))
+                        .route(RouteMapper.map(route, stopDictionary))
                         .trips(tripSet.get(route).stream()
                                 .map(trip -> TripMapper.map(route, trip))
                                 .sorted(Comparator
