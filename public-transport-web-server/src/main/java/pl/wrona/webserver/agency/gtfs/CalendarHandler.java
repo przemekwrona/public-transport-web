97768cdb0df0fb78d0efc3770c028ffb7725be1f
przemekwrona <przemekwrona25@gmail.com>
2025-06-06 15:51:00 +0200

    feat: export calendar service to gtfs

---
+++ public-transport-web-server/src/main/java/pl/wrona/webserver/agency/gtfs/CalendarHandler.java
@@ -0,0 +1,31 @@
+package pl.wrona.webserver.agency.gtfs;
+
+import org.onebusaway.gtfs.model.AgencyAndId;
+import org.onebusaway.gtfs.model.ServiceCalendar;
+import pl.wrona.webserver.agency.calendar.CalendarEntity;
+import pl.wrona.webserver.agency.entity.Agency;
+
+public class CalendarHandler {
+
+    public static ServiceCalendar handle(Agency agency, CalendarEntity calendar) {
+        AgencyAndId agencyAndId = new AgencyAndId();
+//        agencyAndId.setAgencyId(agency.getAgencyCode());
+        agencyAndId.setId(calendar.getCalendarName());
+
+        ServiceCalendar serviceCalendar = new ServiceCalendar();
+        serviceCalendar.setServiceId(agencyAndId);
+        serviceCalendar.setMonday(calendar.isMonday() ? 1 : 0);
+        serviceCalendar.setTuesday(calendar.isTuesday() ? 1 : 0);
+        serviceCalendar.setWednesday(calendar.isWednesday() ? 1 : 0);
+        serviceCalendar.setThursday(calendar.isThursday() ? 1 : 0);
+        serviceCalendar.setFriday(calendar.isFriday() ? 1 : 0);
+        serviceCalendar.setSaturday(calendar.isSaturday() ? 1 : 0);
+        serviceCalendar.setSunday(calendar.isSunday() ? 1 : 0);
+
+        serviceCalendar.setStartDate(calendar.getServiceStartDate());
+        serviceCalendar.setEndDate(calendar.getServiceEndDate());
+
+        return serviceCalendar;
+
+    }
+}
