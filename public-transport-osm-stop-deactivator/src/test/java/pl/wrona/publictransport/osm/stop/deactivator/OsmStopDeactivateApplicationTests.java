17f0e3f8b9e4646629f7b46d647ad45519a6d623
przemekwrona <przemekwrona25@gmail.com>
2025-06-07 15:04:25 +0200

    feat: init module to deactivate osm stops

---
+++ public-transport-osm-stop-deactivator/src/test/java/pl/wrona/publictransport/osm/stop/deactivator/OsmStopDeactivateApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wrona.publictransport.osm.stop.deactivator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsmStopDeactivateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
