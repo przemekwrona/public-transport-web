151b04a76f527dc2797d681d8c2178cd120db17f
przemekwrona <przemekwrona25@gmail.com>
2025-06-07 22:13:36 +0200

    feat: import shared module in stop

--- public-transport-web-client/src/app/agency/stops/stops.module.ts
+++ public-transport-web-client/src/app/agency/stops/stops.module.ts
@@ -3,11 +3,13 @@ import {CommonModule} from '@angular/common';
 import {StopsComponent} from "./stops.component";
 import {StopService} from "../../http/stop.service";
 import {MatIconModule} from "@angular/material/icon";
+import {SharedModule} from "../shared/shared.module";
 
 
 @NgModule({
     imports: [
         CommonModule,
+        SharedModule,
         MatIconModule
     ],
     declarations: [
