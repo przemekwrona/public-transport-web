8feab2b512f68f8780a606a075b5665b4535200c
przemekwrona <przemekwrona25@gmail.com>
2025-06-08 08:53:12 +0200

    feat: edit bus stop in modal

--- public-transport-web-client/src/app/agency/shared/bus-stop-modal-editor/bus-stop-modal-editor.component.ts
+++ public-transport-web-client/src/app/agency/shared/bus-stop-modal-editor/bus-stop-modal-editor.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, inject, model, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+
+export interface BusStopModalEditorData {
+    stopId: number;
+    stopName: string;
+    lon: number;
+    lat: number;
+}
 
 @Component({
-  selector: 'app-bus-stop-modal-editor',
-  templateUrl: './bus-stop-modal-editor.component.html',
-  styleUrl: './bus-stop-modal-editor.component.scss'
+    selector: 'app-bus-stop-modal-editor',
+    templateUrl: './bus-stop-modal-editor.component.html',
+    styleUrl: './bus-stop-modal-editor.component.scss'
 })
-export class BusStopModalEditorComponent {
+export class BusStopModalEditorComponent implements OnInit {
+
+    readonly dialogRef = inject(MatDialogRef<BusStopModalEditorComponent>);
+    readonly data = inject<BusStopModalEditorData>(MAT_DIALOG_DATA);
+
+    ngOnInit(): void {
+    }
+
+    closeModal(selectedStop: BusStopModalEditorData) {
+        this.dialogRef.close(selectedStop);
+    }
 
 }
