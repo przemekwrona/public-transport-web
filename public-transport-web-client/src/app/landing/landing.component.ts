4f0617b7191caf183ae89a77978c77a634b1181e
przemekwrona <przemekwrona25@gmail.com>
2025-06-09 17:36:02 +0200

    feat: prepare mail body

--- public-transport-web-client/src/app/landing/landing.component.ts
+++ public-transport-web-client/src/app/landing/landing.component.ts
@@ -12,6 +12,25 @@ export class LandingComponent implements OnInit {
 
     public now: moment.Moment = moment()
 
+    public NEW_LINE: string = '%0D%0A';
+
+    public title: string = 'Zapytanie dotyczące przygotowania danych do Google Maps';
+    public message: string = 'Dzień dobry,' + this.NEW_LINE + this.NEW_LINE +
+        'trafiłem/am na Państwa stronę i jestem zainteresowany/a usługami związanymi z przygotowaniem danych do udostępnienia w Google Maps.' + this.NEW_LINE +
+        this.NEW_LINE +
+        'Chciałbym/Chciałabym dowiedzieć się więcej na temat:' + this.NEW_LINE +
+        '– Zakresu usług (np. opracowania danych, integracji z API, wsparcia technicznego)' + this.NEW_LINE +
+        '– Procesu współpracy i wyceny' + this.NEW_LINE +
+        '– Terminów realizacji' + this.NEW_LINE +
+        this.NEW_LINE +
+        'Proszę o kontakt, chętnie przedstawię więcej szczegółów.' + this.NEW_LINE +
+        'Z góry dziękuję za odpowiedź.' + this.NEW_LINE +
+        this.NEW_LINE +
+        'Pozdrawiam serdecznie,' + this.NEW_LINE +
+        '[Twoje imię i nazwisko]' + this.NEW_LINE +
+        '[Opcjonalnie: nazwa firmy / stanowisko]' + this.NEW_LINE +
+        '[Telefon – opcjonalnie]';
+
     constructor(private _router: Router, private googleAnalyticsService: GoogleAnalyticsService) {
     }
 
