8d9b91552ca7da4f8fffa578a8f376da78d9224b
przemekwrona <przemekwrona25@gmail.com>
2025-06-15 23:31:30 +0200

    feat: print agencies

--- public-transport-web-client/src/app/agency/profile/profile-list/profile-list.component.ts
+++ public-transport-web-client/src/app/agency/profile/profile-list/profile-list.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CommonModule} from "@angular/common";
+import {ActivatedRoute, RouterModule} from "@angular/router";
+import {AgenciesAdminResponse} from "../../../generated/public-transport";
 
 @Component({
-  selector: 'app-profile-list',
-  standalone: true,
-  imports: [],
-  templateUrl: './profile-list.component.html',
-  styleUrl: './profile-list.component.scss'
+    selector: 'app-profile-list',
+    standalone: true,
+    imports: [
+        CommonModule,
+        RouterModule
+    ],
+    templateUrl: './profile-list.component.html',
+    styleUrl: './profile-list.component.scss'
 })
-export class ProfileListComponent {
+export class ProfileListComponent implements OnInit {
+
+    public agenciesResponse: AgenciesAdminResponse = {} as AgenciesAdminResponse;
+
+    constructor(private route: ActivatedRoute) {
+    }
+
+    ngOnInit(): void {
+        this.route.data.subscribe(data => {
+            this.agenciesResponse = data['agencies'];
+        });
+    }
 
 }
