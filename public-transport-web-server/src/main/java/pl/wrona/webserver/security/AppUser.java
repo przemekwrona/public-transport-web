a2fe3e520b35e17982bd303e92523eaeb07aa6f4
przemekwrona <przemekwrona25@gmail.com>
2025-06-15 13:08:02 +0200

    feat: prevent override hashCode and equals in java

--- public-transport-web-server/src/main/java/pl/wrona/webserver/security/AppUser.java
+++ public-transport-web-server/src/main/java/pl/wrona/webserver/security/AppUser.java
@@ -6,15 +6,14 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import pl.wrona.webserver.agency.entity.Agency;
@@ -24,9 +23,10 @@ import java.util.Collection;
 import java.util.Set;
 
 
-@Data
 @Entity
 @Builder
+@Getter
+@Setter
 @Table(name = "app_user")
 @NoArgsConstructor
 @AllArgsConstructor
