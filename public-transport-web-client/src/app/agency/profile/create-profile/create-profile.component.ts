659450aa6d4abbb906d595fcbfc8a888e60255d9
przemekwrona <przemekwrona25@gmail.com>
2025-06-18 16:49:05 +0200

    feat: validate form

--- public-transport-web-client/src/app/agency/profile/create-profile/create-profile.component.ts
+++ public-transport-web-client/src/app/agency/profile/create-profile/create-profile.component.ts
@@ -2,13 +2,14 @@ import {Component} from '@angular/core';
 import {RouterModule} from "@angular/router";
 import {CommonModule} from "@angular/common";
 import {AgencyAdminCreateAccountRequest, AgencyService} from "../../../generated/public-transport";
-import {FormsModule} from "@angular/forms";
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
     selector: 'app-create-profile',
     standalone: true,
     imports: [
         CommonModule,
+        ReactiveFormsModule,
         RouterModule,
         FormsModule
     ],
@@ -18,16 +19,45 @@ import {FormsModule} from "@angular/forms";
 })
 export class CreateProfileComponent {
 
+    public createProfileForm: FormGroup;
+
     public agency: AgencyAdminCreateAccountRequest = {} as AgencyAdminCreateAccountRequest;
 
     public repeatedPassword: string = '';
 
-    constructor(private agencyService: AgencyService) {
+    constructor(private fb: FormBuilder, private agencyService: AgencyService) {
+        this.createProfileForm = this.fb.group({
+            companyName: ['', [Validators.required, Validators.minLength(3)]],
+            companyCode: ['', [Validators.required, Validators.minLength(3)]],
+            taxIdentificationNumber: ['', [Validators.required, Validators.minLength(3)]],
+            accountName: ['', [Validators.required, Validators.minLength(3)]],
+            accountPassword: ['', [Validators.required]],
+            repeatedPassword: ['', [Validators.required]]
+        });
     }
 
     public createProfile(): void {
-        this.agencyService.createNewAccount(this.agency).subscribe(response => {
-        });
+        if (this.createProfileForm.valid) {
+            this.agencyService.createNewAccount(this.agency).subscribe(response => {
+            });
+        } else {
+            this.createProfileForm.markAllAsTouched();
+        }
+    }
+
+    public validityCheck(fieldName: string): boolean {
+        return this.createProfileForm.get(fieldName)?.touched
+            && this.createProfileForm.get(fieldName)?.invalid;
+    }
+
+    public validityCheckRequired(fieldName: string): boolean {
+        return this.validityCheck(fieldName)
+            && this.createProfileForm.get(fieldName)?.errors?.['required'];
+    }
+
+    public validityCheckMinLength(fieldName: string): boolean {
+        return this.validityCheck(fieldName)
+            && this.createProfileForm.get(fieldName)?.errors?.['minlength'];
     }
 
 }
