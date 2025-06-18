8bf39cb3915156a99eafef9f469c0a85c5b304bf
przemekwrona <przemekwrona25@gmail.com>
2025-06-18 15:45:55 +0200

    feat: adapt create component form to model

--- public-transport-web-client/src/app/agency/profile/create-profile/create-profile.component.ts
+++ public-transport-web-client/src/app/agency/profile/create-profile/create-profile.component.ts
@@ -1,17 +1,33 @@
 import {Component} from '@angular/core';
 import {RouterModule} from "@angular/router";
 import {CommonModule} from "@angular/common";
+import {AgencyAdminCreateAccountRequest, AgencyService} from "../../../generated/public-transport";
+import {FormsModule} from "@angular/forms";
 
 @Component({
     selector: 'app-create-profile',
     standalone: true,
     imports: [
         CommonModule,
-        RouterModule
+        RouterModule,
+        FormsModule
     ],
+    providers: [],
     templateUrl: './create-profile.component.html',
     styleUrl: './create-profile.component.scss'
 })
 export class CreateProfileComponent {
+
+    public agency: AgencyAdminCreateAccountRequest = {} as AgencyAdminCreateAccountRequest;
+
+    public repeatedPassword: string = '';
+
+    constructor(private agencyService: AgencyService) {
+    }
+
+    public createProfile(): void {
+        this.agencyService.createNewAccount(this.agency).subscribe(response => {
+        });
+    }
 
 }
