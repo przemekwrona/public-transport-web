9b971421e168aa7c9d07b5bd6edeee97d8088072
przemekwrona <przemekwrona25@gmail.com>
2025-06-18 15:45:24 +0200

    feat: handle create account in controller

--- public-transport-web-server/src/main/java/pl/wrona/webserver/agency/AgencyController.java
+++ public-transport-web-server/src/main/java/pl/wrona/webserver/agency/AgencyController.java
@@ -3,16 +3,24 @@ package pl.wrona.webserver.agency;
 import lombok.AllArgsConstructor;
 import org.igeolab.iot.pt.server.api.AgencyApi;
 import org.igeolab.iot.pt.server.api.model.AgenciesAdminResponse;
+import org.igeolab.iot.pt.server.api.model.AgencyAdminCreateAccountRequest;
 import org.igeolab.iot.pt.server.api.model.AgencyDetails;
 import org.igeolab.iot.pt.server.api.model.Status;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+import pl.wrona.webserver.bussiness.admin.profile.creator.ProfileCreatorService;
 
 @RestController
 @AllArgsConstructor
 public class AgencyController implements AgencyApi {
 
     private final AgencyService agencyService;
+    private final ProfileCreatorService profileCreatorService;
+
+    @Override
+    public ResponseEntity<Status> createNewAccount(AgencyAdminCreateAccountRequest agencyAdminCreateAccountRequest) {
+        return ResponseEntity.ok(profileCreatorService.createNewAccount(agencyAdminCreateAccountRequest));
+    }
 
     @Override
     public ResponseEntity<AgenciesAdminResponse> findAllAgencies() {
