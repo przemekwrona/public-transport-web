09243818931af348d06000bc019f5a08c87032e2
przemekwrona <przemekwrona25@gmail.com>
2025-06-18 15:03:15 +0200

    feat: project create account component

--- public-transport-web-client/src/app/agency/profile/create-profile/create-profile.component.ts
+++ public-transport-web-client/src/app/agency/profile/create-profile/create-profile.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {RouterModule} from "@angular/router";
+import {CommonModule} from "@angular/common";
 
 @Component({
-  selector: 'app-create-profile',
-  standalone: true,
-  imports: [],
-  templateUrl: './create-profile.component.html',
-  styleUrl: './create-profile.component.scss'
+    selector: 'app-create-profile',
+    standalone: true,
+    imports: [
+        CommonModule,
+        RouterModule
+    ],
+    templateUrl: './create-profile.component.html',
+    styleUrl: './create-profile.component.scss'
 })
 export class CreateProfileComponent {
 
