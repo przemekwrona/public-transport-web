fd8a787f5e67ffbb90a3056de39ac0324e6611c9
przemekwrona <przemekwrona25@gmail.com>
2025-06-28 10:18:20 +0200

    feat: logout button in header

--- public-transport-web-client/src/app/agency/header/header.component.ts
+++ public-transport-web-client/src/app/agency/header/header.component.ts
@@ -1,17 +1,28 @@
-import { Component, HostBinding } from '@angular/core';
+import {Component, HostBinding} from '@angular/core';
+import {AuthService} from "../../auth/auth.service";
+import {TranslocoPipe} from "@jsverse/transloco";
+import {Router} from "@angular/router";
 
 @Component({
-  standalone: true,
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.scss',
-  imports: [
-  ]
+    standalone: true,
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.scss',
+    imports: [TranslocoPipe],
+    providers: [AuthService]
 })
 export class HeaderComponent {
-  @HostBinding('class') hostClass = 'header fixed top-0 z-10 left-0 right-0 flex items-stretch shrink-0 bg-[#fefefe] dark:bg-coal-500 shadow-sm dark:border-b dark:border-b-coal-100';
-  @HostBinding('attr.role') hostRole = 'banner';
-  @HostBinding('attr.data-sticky') dataSticky = 'true';
-  @HostBinding('attr.data-sticky-name') dataStickyName = 'header';
-  @HostBinding('id') hostId = 'header';
+    @HostBinding('class') hostClass = 'header fixed top-0 z-10 left-0 right-0 flex items-stretch shrink-0 bg-[#fefefe] dark:bg-coal-500 shadow-sm dark:border-b dark:border-b-coal-100';
+    @HostBinding('attr.role') hostRole = 'banner';
+    @HostBinding('attr.data-sticky') dataSticky = 'true';
+    @HostBinding('attr.data-sticky-name') dataStickyName = 'header';
+    @HostBinding('id') hostId = 'header';
+
+    constructor(private authService: AuthService, private router: Router) {
+    }
+
+    public logout(): void {
+        this.authService.logout();
+        this.router.navigate(['/signin']).then(() => {});
+    }
 }
