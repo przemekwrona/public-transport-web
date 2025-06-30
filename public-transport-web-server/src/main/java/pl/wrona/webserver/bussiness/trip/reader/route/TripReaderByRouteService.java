dc5da36be006591a8d1bbe30b3da6b559b586a45
przemekwrona <przemekwrona25@gmail.com>
2025-06-30 13:49:09 +0200

    feat: add missing methods

--- public-transport-web-server/src/main/java/pl/wrona/webserver/bussiness/trip/reader/route/TripReaderByRouteService.java
+++ public-transport-web-server/src/main/java/pl/wrona/webserver/bussiness/trip/reader/route/TripReaderByRouteService.java
@@ -1,13 +1,12 @@
 package pl.wrona.webserver.bussiness.trip.reader.route;
 
 import lombok.AllArgsConstructor;
+import org.igeolab.iot.pt.server.api.model.RouteDetails;
 import org.igeolab.iot.pt.server.api.model.RouteId;
-import org.igeolab.iot.pt.server.api.model.Trips;
 import org.springframework.stereotype.Service;
-import pl.wrona.webserver.core.AgencyService;
 import pl.wrona.webserver.core.StopService;
 import pl.wrona.webserver.core.agency.RouteQueryService;
-import pl.wrona.webserver.core.agency.AgencyEntity;
+import pl.wrona.webserver.core.agency.TripQueryService;
 import pl.wrona.webserver.core.mapper.RouteMapper;
 import pl.wrona.webserver.core.mapper.TripMapper;
 
@@ -17,24 +16,20 @@ import java.util.List;
 @AllArgsConstructor
 public class TripReaderByRouteService {
 
-    private final AgencyService agencyService;
-    private final TripReaderByRouteRepository tripReaderByRouteRepository;
     private final RouteQueryService routeQueryService;
+    private final TripQueryService tripQueryService;
     private final StopService stopService;
 
-    public Trips getTrips(RouteId routeId) {
-        AgencyEntity loggedAppUser = agencyService.getLoggedAgency();
-
-        var route = routeQueryService.findRouteByNameAndLine(routeId.getLine(), routeId.getName());
-
-        var trips = tripReaderByRouteRepository.findAllByAgencyAndLineAndName(loggedAppUser, routeId.getLine(), routeId.getName()).stream()
+    public RouteDetails getRouteDetails(String instance, RouteId routeId) {
+        var route = routeQueryService.findRouteByAgencyCodeAndLineAndName(instance, routeId.getLine(), routeId.getName());
+        var trips = tripQueryService.findByAgencyCodeAndLineAndName(instance, routeId.getLine(), routeId.getName()).stream()
                 .map(TripMapper::map)
                 .toList();
 
         var stopsIds = List.of(route.getOriginStopId(), route.getDestinationStopId());
         var stopDictionary = stopService.mapStopByIdsIn(stopsIds);
 
-        return new Trips()
+        return new RouteDetails()
                 .route(RouteMapper.map(route, stopDictionary))
                 .trips(trips);
     }
