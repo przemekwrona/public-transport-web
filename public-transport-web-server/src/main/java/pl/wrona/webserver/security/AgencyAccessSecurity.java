902c0be4cdb8ded4ed34b856c0eb3418e173554c
przemekwrona <przemekwrona25@gmail.com>
2025-06-30 12:37:05 +0200

    feat: secure annotation

---
+++ public-transport-web-server/src/main/java/pl/wrona/webserver/security/AgencyAccessSecurity.java
@@ -0,0 +1,19 @@
+package pl.wrona.webserver.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import pl.wrona.webserver.core.agency.AgencyEntity;
+
+import java.util.stream.Collectors;
+
+@Component
+public class AgencyAccessSecurity {
+
+    public boolean checkAccess(String agencyCode) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        AppUser appUser = (AppUser) authentication.getPrincipal();
+
+        return appUser.getAgencies().stream().map(AgencyEntity::getAgencyCode).collect(Collectors.toSet()).contains(agencyCode);
+    }
+}
