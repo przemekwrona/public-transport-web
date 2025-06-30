6c465e56746c825ca3e92728a9f171dc98fe0cf4
przemekwrona <przemekwrona25@gmail.com>
2025-06-30 21:45:44 +0200

    feat: add address to agency

--- public-transport-web-server/src/main/resources/db/init_user.sql
+++ public-transport-web-server/src/main/resources/db/init_user.sql
@@ -2,7 +2,7 @@ INSERT INTO app_role (app_role_id, description) VALUES ('SUPER_USER', 'Role for 
 INSERT INTO app_role (app_role_id, description) VALUES ('AGENCY_OWNER', 'Role for the owner of a transit agency');
 
 INSERT INTO app_user (username, email, password, created_at, updated_at, account_non_expired, account_non_locked, credentials_non_expired, enabled)
-VALUES ( 'pwrona', 'biuro@nastepnastacja.pl', '$2a$10$yPoPJ9H4MAm5PP/.hIU/uebkqnFMiTGHb3/I.AMqpkDILHdwxi456', '2025-01-04 11:36:40.937926', '2025-01-04 11:36:40.937949', true, true, true, true);
+VALUES ( 'pwrona', 'biuro@nastepnastacja.pl', '$2a$10$yPoPJ9H4MAm5PP/.hIU/uebkqnFMiTGHb3/I.AMqpkDILHdwxi456', now(), now(), true, true, true, true);
 
 INSERT INTO app_user_app_role(app_user_id, app_role_id) VALUES ((SELECT app_user_id FROM app_user WHERE username = 'pwrona'), 'SUPER_USER');
 INSERT INTO app_user_app_role(app_user_id, app_role_id) VALUES ((SELECT app_user_id FROM app_user WHERE username = 'pwrona'), 'AGENCY_OWNER');
