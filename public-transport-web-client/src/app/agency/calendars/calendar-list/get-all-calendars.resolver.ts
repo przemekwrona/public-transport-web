d6ed97e6362417098b818f9931d3c044cb78f5cb
przemekwrona <przemekwrona25@gmail.com>
2025-06-30 12:14:22 +0200

    feat: add instance to request

--- public-transport-web-client/src/app/agency/calendars/calendar-list/get-all-calendars.resolver.ts
+++ public-transport-web-client/src/app/agency/calendars/calendar-list/get-all-calendars.resolver.ts
@@ -3,7 +3,7 @@ import {inject} from "@angular/core";
 import {Observable} from "rxjs";
 import {CalendarService, GetCalendarsResponse} from "../../../generated/public-transport";
 
-export const getAllCalendarsResolver: ResolveFn<Observable<GetCalendarsResponse>> = (route, state): Observable<GetCalendarsResponse> => {
+export const getAllCalendarsResolver: ResolveFn<GetCalendarsResponse> = (route, state): Observable<GetCalendarsResponse> => {
     const brigadeService = inject(CalendarService);
 
     return brigadeService.getCalendars();
