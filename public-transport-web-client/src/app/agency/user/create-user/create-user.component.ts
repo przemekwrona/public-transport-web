2c1e108ccb041988dec06fd54c2af77c8f067e76
przemekwrona <przemekwrona25@gmail.com>
2025-07-01 09:35:16 +0200

    feat: style form to save user

--- public-transport-web-client/src/app/agency/user/create-user/create-user.component.ts
+++ public-transport-web-client/src/app/agency/user/create-user/create-user.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {RouterModule} from "@angular/router";
+import {CommonModule} from "@angular/common";
 
 @Component({
-  selector: 'app-create-user',
-  standalone: true,
-  imports: [],
-  templateUrl: './create-user.component.html',
-  styleUrl: './create-user.component.scss'
+    selector: 'app-create-user',
+    standalone: true,
+    imports: [
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        RouterModule
+    ],
+    templateUrl: './create-user.component.html',
+    styleUrl: './create-user.component.scss'
 })
 export class CreateUserComponent {
+
+    public createUserForm: FormGroup;
+
+    constructor(private fb: FormBuilder) {
+        this.createUserForm = this.fb.group({
+            accountName: ['', [Validators.required]],
+            accountPassword: ['', [Validators.required]],
+            repeatedPassword: ['', [Validators.required]]
+        });
+    }
+
+    public validityCheck(fieldName: string): boolean {
+        return this.createUserForm.get(fieldName)?.touched
+            && this.createUserForm.get(fieldName)?.invalid;
+    }
+
+    public validityCheckRequired(fieldName: string): boolean {
+        return this.validityCheck(fieldName)
+            && this.createUserForm.get(fieldName)?.errors?.['required'];
+    }
+
+    public createUser(): void {
+
+    }
 
 }
