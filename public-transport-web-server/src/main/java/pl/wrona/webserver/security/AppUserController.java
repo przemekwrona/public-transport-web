b1fb100a77b15e7b55a61bc40dd1a1d7261a8b2f
przemekwrona <przemekwrona25@gmail.com>
2025-10-13 11:49:06 +0200

    feat: add app proper to define /api/v1 prefix

--- public-transport-web-server/src/main/java/pl/wrona/webserver/security/AppUserController.java
+++ public-transport-web-server/src/main/java/pl/wrona/webserver/security/AppUserController.java
@@ -2,15 +2,15 @@ package pl.wrona.webserver.security;
 
 import lombok.AllArgsConstructor;
 import org.igeolab.iot.pt.server.api.AuthApi;
-import org.igeolab.iot.pt.server.api.model.CreateAppUserRequest;
-import org.igeolab.iot.pt.server.api.model.CreateAppUserResponse;
 import org.igeolab.iot.pt.server.api.model.LoginAppUserRequest;
 import org.igeolab.iot.pt.server.api.model.LoginAppUserResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("${webserver.context.path}")
 public class AppUserController implements AuthApi {
 
     private final AppUserService appUserService;
