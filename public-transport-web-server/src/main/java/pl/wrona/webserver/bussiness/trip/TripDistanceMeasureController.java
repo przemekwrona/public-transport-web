0c3708228c38845f89043bf3ff5114010ec198b2
przemekwrona <przemekwrona25@gmail.com>
2025-10-15 10:50:53 +0200

    feat: add @RequestMapping to /api/v1

--- public-transport-web-server/src/main/java/pl/wrona/webserver/bussiness/trip/TripDistanceMeasureController.java
+++ public-transport-web-server/src/main/java/pl/wrona/webserver/bussiness/trip/TripDistanceMeasureController.java
@@ -1,13 +1,16 @@
-package pl.wrona.webserver.bussiness.trip.measure;
+package pl.wrona.webserver.bussiness.trip;
 
 import lombok.AllArgsConstructor;
 import org.igeolab.iot.pt.server.api.TripDistanceMeasuresApi;
 import org.igeolab.iot.pt.server.api.model.Trip;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.wrona.webserver.bussiness.trip.measure.TripDistanceMeasureService;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("${webserver.context.path}")
 public class TripDistanceMeasureController implements TripDistanceMeasuresApi {
 
     private final TripDistanceMeasureService tripDistanceMeasureService;
