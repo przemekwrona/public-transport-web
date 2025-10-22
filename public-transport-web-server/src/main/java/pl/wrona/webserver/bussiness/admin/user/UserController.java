5861ac4fc4de7cdaadf7213016850066a8c41b6a
przemekwrona <przemekwrona25@gmail.com>
2025-10-22 22:55:39 +0200

    feat: @RequestMapping to UserController

--- public-transport-web-server/src/main/java/pl/wrona/webserver/bussiness/admin/user/UserController.java
+++ public-transport-web-server/src/main/java/pl/wrona/webserver/bussiness/admin/user/UserController.java
@@ -6,13 +6,14 @@ import org.igeolab.iot.pt.server.api.model.AppUsersResponse;
 import org.igeolab.iot.pt.server.api.model.CreateAppUserRequest;
 import org.igeolab.iot.pt.server.api.model.CreateAppUserResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.wrona.webserver.bussiness.admin.user.creator.UserCreatorService;
 import pl.wrona.webserver.bussiness.admin.user.pagination.UserPaginationService;
-import pl.wrona.webserver.security.AppUserService;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("${webserver.context.path}")
 public class UserController implements UsersApi {
 
     private final UserPaginationService userPaginationService;
