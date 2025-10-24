8a20737447a5f8fe5a2c49a741f87768919ae218
przemekwrona <przemekwrona25@gmail.com>
2025-10-24 12:41:40 +0200

    feat: set agency cords on frontend

--- public-transport-web-client/src/app/agency/routes/create-route/create-route.component.ts
+++ public-transport-web-client/src/app/agency/routes/create-route/create-route.component.ts
@@ -1,16 +1,17 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ModificationRouteResponse, Route, RouteService, Status, Stop} from "../../../generated/public-transport-api";
 import {Router} from "@angular/router";
 import {BusStopSelectorData} from "../../shared/bus-stop-selector/bus-stop-selector.component";
 import {LoginService} from "../../../auth/login.service";
 import {NotificationService} from "../../../shared/notification.service";
+import {AgencyStorageService} from "../../../auth/agency-storage.service";
 
 @Component({
     selector: 'app-create-route',
     templateUrl: './create-route.component.html',
     styleUrl: './create-route.component.scss'
 })
-export class CreateRouteComponent {
+export class CreateRouteComponent implements OnInit {
 
     public route: Route = {
         google: false,
@@ -20,7 +21,17 @@ export class CreateRouteComponent {
     public origin: BusStopSelectorData = {} as BusStopSelectorData;
     public destination: BusStopSelectorData = {} as BusStopSelectorData;
 
-    constructor(private _router: Router, private routeService: RouteService, private authService: LoginService, private notificationService: NotificationService) {
+    constructor(private _router: Router, private routeService: RouteService, private agencyStorageService: AgencyStorageService, private notificationService: NotificationService) {
+    }
+
+    ngOnInit(): void {
+        this.agencyStorageService.getAgencyAddress().subscribe(agencyAddress => {
+            this.origin.stopLon = agencyAddress.lon;
+            this.origin.stopLat = agencyAddress.lat;
+
+            this.destination.stopLon = agencyAddress.lon;
+            this.destination.stopLat = agencyAddress.lat;
+        });
     }
 
     public createRouteAndNavigateToCreateNewTrip(): void {
@@ -35,7 +46,7 @@ export class CreateRouteComponent {
         this.route.originStop = originStop;
         this.route.destinationStop = destinationStop;
 
-        this.routeService.createRoute(this.authService.getInstance(), this.route).subscribe((response: ModificationRouteResponse) => {
+        this.routeService.createRoute(this.agencyStorageService.getInstance(), this.route).subscribe((response: ModificationRouteResponse) => {
             this.notificationService.showSuccess(`Linia ${this.route.line} (${this.route.name}) została pomyślnie utworzona`);
             this._router.navigate(['/agency/trips/create'], {
                 queryParams: {
@@ -45,4 +56,6 @@ export class CreateRouteComponent {
             }).then();
         });
     }
+
+
 }
