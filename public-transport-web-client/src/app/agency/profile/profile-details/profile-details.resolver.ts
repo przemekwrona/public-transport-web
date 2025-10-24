7a3b6a9ac7d83975d0fb14e7c1ef83dc2d85afc7
przemekwrona <przemekwrona25@gmail.com>
2025-10-24 12:40:58 +0200

    feat: storage agency address in agency storage

--- public-transport-web-client/src/app/agency/profile/profile-details/profile-details.resolver.ts
+++ public-transport-web-client/src/app/agency/profile/profile-details/profile-details.resolver.ts
@@ -2,10 +2,9 @@ import { ResolveFn } from '@angular/router';
 import {AgencyDetails, AgencyService} from "../../../generated/public-transport-api";
 import {inject} from "@angular/core";
 import {LoginService} from "../../../auth/login.service";
+import {AgencyStorageService} from "../../../auth/agency-storage.service";
 
 export const profileDetailsResolver: ResolveFn<AgencyDetails> = (route, state) => {
-  const authService: LoginService = inject(LoginService);
-  const agencyService: AgencyService = inject(AgencyService);
-
-  return agencyService.getAgency(authService.getInstance());
+  const agencyStorageService: AgencyStorageService = inject(AgencyStorageService);
+  return agencyStorageService.getAgency();
 };
--- public-transport-web-client/src/app/auth/agency-storage.service.ts
+++ public-transport-web-client/src/app/auth/agency-storage.service.ts
@@ -1,19 +1,37 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {AgencyAddress, AgencyDetails, AgencyService} from "../generated/public-transport-api";
+import {map, Observable, of, tap} from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AgencyStorageService {
 
-  static SESSION_STORAGE_INSTANCE_KEY = 'instance';
+    static SESSION_STORAGE_INSTANCE_KEY = 'instance';
 
-  constructor() { }
+    private agencyDetails: AgencyDetails = null;
 
-  public getInstance(): string {
-    return sessionStorage.getItem(AgencyStorageService.SESSION_STORAGE_INSTANCE_KEY) || '';
-  }
+    constructor(private agencyService: AgencyService) {
+    }
 
-  public setInstance(instance: string): void {
-    sessionStorage.setItem(AgencyStorageService.SESSION_STORAGE_INSTANCE_KEY, instance || '');
-  }
+    public getInstance(): string {
+        return sessionStorage.getItem(AgencyStorageService.SESSION_STORAGE_INSTANCE_KEY) || '';
+    }
+
+    public setInstance(instance: string): void {
+        sessionStorage.setItem(AgencyStorageService.SESSION_STORAGE_INSTANCE_KEY, instance || '');
+    }
+
+    public getAgency(): Observable<AgencyDetails> {
+        if (this.agencyDetails == null) {
+            return this.agencyService.getAgency(this.getInstance())
+                .pipe(tap(agencyDetails => this.agencyDetails = agencyDetails));
+        } else {
+            return of(this.agencyDetails);
+        }
+    }
+
+    public getAgencyAddress(): Observable<AgencyAddress> {
+        return this.getAgency().pipe(map(agencyDetails => agencyDetails.agencyAddress));
+    }
 }
