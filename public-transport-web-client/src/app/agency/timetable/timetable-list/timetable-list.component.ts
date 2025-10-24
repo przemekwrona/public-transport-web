77cecfe7e080b75fc5aab6b698678c0a56cdeb25
przemekwrona <przemekwrona25@gmail.com>
2025-10-24 12:51:29 +0200

    feat: init empty timetable component

---
+++ public-transport-web-client/src/app/agency/timetable/timetable-list/timetable-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-timetable-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './timetable-list.component.html',
+  styleUrl: './timetable-list.component.css'
+})
+export class TimetableListComponent {
+
+}
