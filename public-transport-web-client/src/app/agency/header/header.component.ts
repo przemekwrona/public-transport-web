a6badf65f69a08d840cd183db0e2445e449d2c4a
przemekwrona <przemekwrona25@gmail.com>
2025-10-28 09:37:07 +0100

    feat: replace mock by agency name in header

--- public-transport-web-client/src/app/agency/header/header.component.ts
+++ public-transport-web-client/src/app/agency/header/header.component.ts
@@ -1,28 +1,38 @@
-import {Component, HostBinding} from '@angular/core';
+import {Component, HostBinding, OnInit} from '@angular/core';
 import {LoginService} from "../../auth/login.service";
 import {TranslocoPipe} from "@jsverse/transloco";
 import {Router} from "@angular/router";
+import {AgencyStorageService} from "../../auth/agency-storage.service";
+import {AgencyDetails} from "../../generated/public-transport-api";
+import {CommonModule} from "@angular/common";
 
 @Component({
     standalone: true,
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrl: './header.component.scss',
-    imports: [TranslocoPipe],
+    imports: [CommonModule, TranslocoPipe],
     providers: [LoginService]
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
     @HostBinding('class') hostClass = 'header fixed top-0 z-10 left-0 right-0 flex items-stretch shrink-0 bg-[#fefefe] dark:bg-coal-500 shadow-sm dark:border-b dark:border-b-coal-100';
     @HostBinding('attr.role') hostRole = 'banner';
     @HostBinding('attr.data-sticky') dataSticky = 'true';
     @HostBinding('attr.data-sticky-name') dataStickyName = 'header';
     @HostBinding('id') hostId = 'header';
 
-    constructor(private authService: LoginService, private router: Router) {
+    public agencyDetails: AgencyDetails;
+
+    constructor(private authService: LoginService, private router: Router, private agencyStorageService: AgencyStorageService) {
+    }
+
+    ngOnInit(): void {
+        this.agencyStorageService.getAgency().subscribe(agencyDetails => this.agencyDetails = agencyDetails);
     }
 
     public logout(): void {
         this.authService.logout();
-        this.router.navigate(['/signin']).then(() => {});
+        this.router.navigate(['/signin']).then(() => {
+        });
     }
 }
