439cc9faf673f41562dc39aad132ef699c41c6b7
przemekwrona <przemekwrona25@gmail.com>
2025-10-30 13:25:52 +0100

    feat: refactor trip query service

--- public-transport-web-server/src/main/java/pl/wrona/webserver/bussiness/trip/TripQueryRepository.java
+++ public-transport-web-server/src/main/java/pl/wrona/webserver/bussiness/trip/TripQueryRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pl.wrona.webserver.core.agency.TripEntity;
+import pl.wrona.webserver.core.agency.TripTrafficMode;
+import pl.wrona.webserver.core.agency.TripVariantMode;
 
 import java.util.List;
 
@@ -15,9 +17,24 @@ public interface TripQueryRepository extends JpaRepository<TripEntity, Long> {
     List<TripEntity> findByAgencyCodeAndLineAndName(@Param("agencyCode") String agencyCode, @Param("line") String line, @Param("name") String name);
 
     @Query("""
+            SELECT CASE WHEN (COUNT(*) > 0) THEN TRUE ELSE FALSE END
+            FROM TripEntity t
+            WHERE t.route.agency.agencyCode = :agencyCode
+            AND t.route.line = :line
+            AND t.route.name = :name
+            AND t.mode = :mode
+            AND t.trafficMode = :trafficMode""")
+    boolean existsTripUniqueIndex(@Param("agencyCode") String agencyCode, @Param("line") String line, @Param("name") String name, @Param("mode") TripVariantMode mode, @Param("trafficMode") TripTrafficMode trafficMode);
+
+    @Query("""
+            SELECT CASE WHEN (COUNT(*) > 0) THEN TRUE ELSE FALSE END
+            FROM BrigadeTripEntity b
+            WHERE b.rootTrip = :trip""")
+    boolean existsTripInBrigade(@Param("trip") TripEntity trip);
+
+    @Query("""
             SELECT t FROM TripEntity t
             WHERE t.route.agency.agencyCode = :agencyCode
                 AND EXISTS (SELECT 1 FROM BrigadeTripEntity bd WHERE bd.rootTrip.tripId = t.tripId)""")
     List<TripEntity> findByExistsBrigade(@Param("agencyCode") String agencyCode);
-
 }
--- public-transport-web-server/src/main/java/pl/wrona/webserver/bussiness/trip/TripQueryService.java
+++ public-transport-web-server/src/main/java/pl/wrona/webserver/bussiness/trip/TripQueryService.java
@@ -3,6 +3,8 @@ package pl.wrona.webserver.bussiness.trip;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import pl.wrona.webserver.core.agency.TripEntity;
+import pl.wrona.webserver.core.agency.TripTrafficMode;
+import pl.wrona.webserver.core.agency.TripVariantMode;
 
 import java.util.List;
 import java.util.Map;
@@ -19,8 +21,16 @@ public class TripQueryService {
         return tripQueryRepository.findByAgencyCodeAndLineAndName(agencyCode, line, name);
     }
 
+    public boolean existsUniqueTripIndex(String agencyCode, String line, String name, TripVariantMode tripMode, TripTrafficMode trafficMode) {
+        return tripQueryRepository.existsTripUniqueIndex(agencyCode, line, name, tripMode, trafficMode);
+    }
+
     public List<TripEntity> findByExistsBrigade(String agency) {
         return tripQueryRepository.findByExistsBrigade(agency);
+    }
+
+    public boolean existsTripInBrigade(TripEntity trip) {
+        return tripQueryRepository.existsTripInBrigade(trip);
     }
 
     public Map<Long, TripEntity> mapByExistsBrigade(String agency) {
