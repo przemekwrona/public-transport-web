f39bbb86f26fc1039e289e3164adf6af07c42933
przemekwrona <przemekwrona25@gmail.com>
2025-11-18 13:45:39 +0100

    feat: crop image by modal

--- public-transport-web-client/src/app/agency/shared/crop-photo-modal/crop-photo-modal.component.ts
+++ public-transport-web-client/src/app/agency/shared/crop-photo-modal/crop-photo-modal.component.ts
@@ -1,39 +1,59 @@
 import {Component, inject, OnInit} from '@angular/core';
 import {CommonModule} from "@angular/common";
 import {MAT_DIALOG_DATA, MatDialogModule, MatDialogRef} from "@angular/material/dialog";
-import {BusStopEditorComponent} from "../bus-stop-editor/bus-stop-editor.component";
-import {StopsPatchRequest} from "../../../generated/public-transport-api";
-import {BusStopModalEditorData} from "../bus-stop-modal-editor/bus-stop-modal-editor.component";
+import {ImageCroppedEvent, ImageCropperComponent, LoadedImage} from "ngx-image-cropper";
+
+export interface ImageEvent {
+    event: Event;
+}
 
 @Component({
-  selector: 'app-crop-photo-modal',
-  standalone: true,
-  imports: [
-    CommonModule,
-    MatDialogModule,
-    BusStopEditorComponent
-  ],
-  templateUrl: './crop-photo-modal.component.html',
-  styleUrl: './crop-photo-modal.component.scss'
+    selector: 'app-crop-photo-modal',
+    standalone: true,
+    imports: [
+        CommonModule,
+        MatDialogModule,
+        ImageCropperComponent
+    ],
+    templateUrl: './crop-photo-modal.component.html',
+    styleUrl: './crop-photo-modal.component.scss'
 })
 export class CropPhotoModalComponent implements OnInit {
 
-  readonly dialogRef = inject(MatDialogRef<Blob>);
-  readonly data = inject<BusStopModalEditorData>(MAT_DIALOG_DATA);
+    readonly dialogRef = inject(MatDialogRef<CropPhotoModalComponent>);
+    readonly data: ImageEvent = inject<ImageEvent>(MAT_DIALOG_DATA);
 
-  ngOnInit(): void {
-  }
+    private imageChangedEvent: ImageCroppedEvent;
 
-  forceCloseModal() {
-    this.dialogRef.close();
-  }
+    constructor() {
+    }
 
-  closeModal(event: StopsPatchRequest) {
-    const busStop: BusStopModalEditorData = {} as BusStopModalEditorData;
-    busStop.stopId = event.id;
-    busStop.stopName = event.name;
-    this.dialogRef.close(busStop);
-  }
+    ngOnInit(): void {
+    }
+
+    forceCloseModal() {
+        this.dialogRef.close();
+    }
+
+    closeModal() {
+        this.dialogRef.close(this.imageChangedEvent);
+    }
+
+    async imageCropped(event: ImageCroppedEvent) {
+        this.imageChangedEvent = event;
+    }
+
+    imageLoaded(image: LoadedImage) {
+        // show cropper
+    }
+
+    cropperReady() {
+        // cropper ready
+    }
+
+    loadImageFailed() {
+        // show message
+    }
 
 
 }
