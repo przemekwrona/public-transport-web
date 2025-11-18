a70980d2e92d5c55b3d2d98238d84ce52e535c98
przemekwrona <przemekwrona25@gmail.com>
2025-11-18 02:22:25 +0100

    feat: save and get profile photo

--- public-transport-web-server/src/main/java/pl/wrona/webserver/bussiness/agency/photo/AgencyPhotoService.java
+++ public-transport-web-server/src/main/java/pl/wrona/webserver/bussiness/agency/photo/AgencyPhotoService.java
@@ -2,26 +2,55 @@ package pl.wrona.webserver.bussiness.agency.photo;
 
 import lombok.AllArgsConstructor;
 import org.igeolab.iot.pt.server.api.model.Status;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+import pl.wrona.webserver.core.AgencyPhotoRepository;
 import pl.wrona.webserver.core.AgencyRepository;
 import pl.wrona.webserver.core.agency.AgencyEntity;
+import pl.wrona.webserver.core.agency.AgencyPhotoEntity;
+import pl.wrona.webserver.exception.BusinessException;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 @Service
 @AllArgsConstructor
 public class AgencyPhotoService {
 
     private AgencyRepository agencyRepository;
+    private AgencyPhotoRepository agencyPhotoRepository;
 
-    public Status putAgencyPhoto(String agency, MultipartFile file) {
-        AgencyEntity agencyEntity = agencyRepository.findByAgencyCodeEquals(agency);
+    public Status putAgencyPhoto(String instance, MultipartFile file) {
+        AgencyEntity agencyEntity = agencyRepository.findByAgencyCodeEquals(instance);
+
         try {
-            agencyEntity.setPhoto(file.getBytes());
-            agencyRepository.save(agencyEntity);
+            AgencyPhotoEntity agencyPhoto = new AgencyPhotoEntity();
+            agencyPhoto.setPhoto(file.getBytes());
+            agencyPhoto.setContentType(file.getContentType());
+            agencyPhoto.setCreatedAt(LocalDateTime.now());
+            agencyPhoto.setAgency(agencyEntity);
+            agencyPhotoRepository.save(agencyPhoto);
         } catch (IOException exception) {
+            throw new BusinessException("ERROR:202511150747", exception.getMessage());
         }
         return new Status().status(Status.StatusEnum.SUCCESS);
+    }
+
+    @Transactional(readOnly = true)
+    public ResponseEntity<Resource> getAgencyPhoto(String instance) throws IOException {
+        AgencyEntity agencyEntity = agencyRepository.findByAgencyCodeEquals(instance);
+        var photo = agencyPhotoRepository.findFirstByAgencyOrderByCreatedAtDesc(agencyEntity);
+
+        Resource resource = new ByteArrayResource(photo.getPhoto());
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(photo.getContentType()))
+                .contentLength(resource.contentLength())
+                .body(resource);
     }
 }
---
+++ public-transport-web-server/src/main/java/pl/wrona/webserver/core/AgencyPhotoRepository.java
@@ -0,0 +1,12 @@
+package pl.wrona.webserver.core;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.wrona.webserver.core.agency.AgencyEntity;
+import pl.wrona.webserver.core.agency.AgencyPhotoEntity;
+
+@Repository
+public interface AgencyPhotoRepository extends JpaRepository<AgencyPhotoEntity, Long> {
+
+    AgencyPhotoEntity findFirstByAgencyOrderByCreatedAtDesc(AgencyEntity agency);
+}
