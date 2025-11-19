14446fcf04988a10ef26b57b746945ebd641ea8e
przemekwrona <przemekwrona25@gmail.com>
2025-11-19 18:19:29 +0100

    feat: prevent save file above 2MB

--- public-transport-web-client/src/app/agency/shared/crop-photo-modal/crop-photo-modal.component.ts
+++ public-transport-web-client/src/app/agency/shared/crop-photo-modal/crop-photo-modal.component.ts
@@ -1,7 +1,8 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {Component, inject, OnInit, Sanitizer} from '@angular/core';
 import {CommonModule} from "@angular/common";
 import {MAT_DIALOG_DATA, MatDialogModule, MatDialogRef} from "@angular/material/dialog";
 import {ImageCroppedEvent, ImageCropperComponent, LoadedImage} from "ngx-image-cropper";
+import {DomSanitizer, SafeUrl} from "@angular/platform-browser";
 
 export interface ImageEvent {
     event: Event;
@@ -20,27 +21,34 @@ export interface ImageEvent {
 })
 export class CropPhotoModalComponent implements OnInit {
 
+    private readonly _1MB = 1024 * 1024;
+    private readonly _2MB = 2 * 1024 * 1024;
+
     readonly dialogRef = inject(MatDialogRef<CropPhotoModalComponent>);
     readonly data: ImageEvent = inject<ImageEvent>(MAT_DIALOG_DATA);
 
-    private imageChangedEvent: ImageCroppedEvent;
+    public imageChangedEvent: ImageCroppedEvent;
+    public croppedImage: SafeUrl;
 
-    constructor() {
+    constructor(private sanitizer: DomSanitizer) {
     }
 
     ngOnInit(): void {
     }
 
     forceCloseModal() {
-        this.dialogRef.close();
+        this.dialogRef.close(null);
     }
 
-    closeModal() {
-        this.dialogRef.close(this.imageChangedEvent);
+    closeModal(): void {
+        if (this.getFileSize() <= this._2MB) {
+            this.dialogRef.close(this.imageChangedEvent);
+        }
     }
 
-    async imageCropped(event: ImageCroppedEvent) {
+    imageCropped(event: ImageCroppedEvent) {
         this.imageChangedEvent = event;
+        this.croppedImage = this.sanitizer.bypassSecurityTrustUrl(event.objectUrl);
     }
 
     imageLoaded(image: LoadedImage) {
@@ -55,5 +63,12 @@ export class CropPhotoModalComponent implements OnInit {
         // show message
     }
 
+    public getFileSize(): number {
+        return (this.imageChangedEvent?.blob?.size || 0) / this._1MB;
+    }
+
+    public hasSizeGtOrEqThan2MB(): boolean {
+        return (this.imageChangedEvent?.blob?.size || 0) >= this._2MB;
+    }
 
 }
