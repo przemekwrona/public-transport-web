6415ab6217c68932ce5208de72fa0dae317c1c12
przemekwrona <przemekwrona25@gmail.com>
2025-11-20 00:49:21 +0100

    feat: init create timetable board

---
+++ public-transport-web-client/src/app/agency/timetable/create-timetable/timetable-board/timetable-board.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-timetable-board',
+  standalone: true,
+  imports: [],
+  templateUrl: './timetable-board.component.html',
+  styleUrl: './timetable-board.component.scss'
+})
+export class TimetableBoardComponent {
+
+}
