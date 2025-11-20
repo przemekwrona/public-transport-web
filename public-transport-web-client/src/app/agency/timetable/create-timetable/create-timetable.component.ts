112ac9cc7e7a5acf81cbe41326d3c2bd61741127
przemekwrona <przemekwrona25@gmail.com>
2025-11-20 01:50:58 +0100

    feat: add departure board

--- public-transport-web-client/src/app/agency/timetable/create-timetable/create-timetable.component.ts
+++ public-transport-web-client/src/app/agency/timetable/create-timetable/create-timetable.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {TimetableBoardComponent} from "./timetable-board/timetable-board.component";
 
 @Component({
-  selector: 'app-create-timetable',
-  standalone: true,
-  imports: [],
-  templateUrl: './create-timetable.component.html',
-  styleUrl: './create-timetable.component.scss'
+    selector: 'app-create-timetable',
+    standalone: true,
+    imports: [
+        TimetableBoardComponent
+    ],
+    templateUrl: './create-timetable.component.html',
+    styleUrl: './create-timetable.component.scss'
 })
 export class CreateTimetableComponent {
 
