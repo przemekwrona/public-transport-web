63af708da2eab3545cb5011f7617a41a6e957d52
przemekwrona <przemekwrona25@gmail.com>
2025-11-24 14:34:04 +0100

    feat: implement check trip details existence

---
+++ public-transport-web-server/src/main/java/pl/wrona/webserver/bussiness/trip/existance/TripExistenceService.java
@@ -0,0 +1,24 @@
+package pl.wrona.webserver.bussiness.trip.existance;
+
+import lombok.AllArgsConstructor;
+import org.igeolab.iot.pt.server.api.model.Status;
+import org.igeolab.iot.pt.server.api.model.TripId;
+import org.springframework.stereotype.Service;
+import pl.wrona.webserver.bussiness.trip.TripQueryRepository;
+import pl.wrona.webserver.core.mapper.TripModeMapper;
+import pl.wrona.webserver.core.mapper.TripTrafficModeMapper;
+import pl.wrona.webserver.security.PreAgencyAuthorize;
+
+@Service
+@AllArgsConstructor
+public class TripExistenceService {
+
+    private final TripQueryRepository tripQueryRepository;
+
+
+    @PreAgencyAuthorize
+    public Status hasVariantDetails(String agency, TripId tripId) {
+        boolean variantAlreadyExists = tripQueryRepository.existsTripUniqueIndex(agency, tripId.getRouteId().getLine(), tripId.getRouteId().getName(), TripModeMapper.map(tripId.getMode()), TripTrafficModeMapper.map(tripId.getTrafficMode()));
+        return variantAlreadyExists ? new Status().status(Status.StatusEnum.EXISTS) : new Status().status(Status.StatusEnum.NOT_EXIST);
+    }
+}
