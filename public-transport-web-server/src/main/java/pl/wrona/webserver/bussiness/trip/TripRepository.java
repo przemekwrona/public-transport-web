8b3e9520e03c74998eb522bb3a6801833b340884
przemekwrona <przemekwrona25@gmail.com>
2025-11-24 14:31:31 +0100

    feat: remove unused code TripService::findAllByRouteAndVariantNameAndMode

--- public-transport-web-server/src/main/java/pl/wrona/webserver/bussiness/trip/TripRepository.java
+++ public-transport-web-server/src/main/java/pl/wrona/webserver/bussiness/trip/TripRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pl.wrona.webserver.core.agency.AgencyEntity;
-import pl.wrona.webserver.core.agency.RouteEntity;
 import pl.wrona.webserver.core.agency.TripEntity;
 import pl.wrona.webserver.core.agency.TripVariantMode;
 
@@ -16,8 +15,6 @@ public interface TripRepository extends JpaRepository<TripEntity, Long> {
 
     @Query("SELECT t FROM TripEntity t WHERE t.route.agency = :agency AND t.route.line LIKE CONCAT(:lineOrName,'%') OR t.route.name = CONCAT(:lineOrName,'%')")
     List<TripEntity> findByLineOrNameContainingIgnoreCase(@Param("lineOrName") String lineOrName, @Param("agency") AgencyEntity agencyEntity);
-
-    TripEntity findAllByRouteAndVariantNameAndMode(RouteEntity routeEntity, String variantName, TripVariantMode mode);
 
     @Query("SELECT t FROM TripEntity t WHERE t.route.line = :line AND t.route.name = :name AND t.variantName = :variantName AND t.mode = :mode")
     TripEntity findByLineAndNameAndVariantAndMode(@Param("line") String line, @Param("name") String name, @Param("variantName") String variantName, @Param("mode") TripVariantMode mode);
