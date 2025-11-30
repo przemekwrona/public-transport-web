b13172d377c931e648d153dca9b3ec4d29219451
przemekwrona <przemekwrona25@gmail.com>
2025-11-30 14:00:53 +0100

    feat: not Fount -> no Content

--- public-transport-web-server/src/main/java/pl/wrona/webserver/bussiness/agency/photo/AgencyPhotoService.java
+++ public-transport-web-server/src/main/java/pl/wrona/webserver/bussiness/agency/photo/AgencyPhotoService.java
@@ -71,7 +71,7 @@ public class AgencyPhotoService {
                     }
                 })
                 .orElse(ResponseEntity
-                        .notFound()
+                        .noContent()
                         .build());
     }
 }
