262132e5f0ce4f9071bb4bcfa6dfa15cf9d6fdea
przemekwrona <przemekwrona25@gmail.com>
2025-12-25 17:31:33 +0100

    feat: generate timetable

--- public-transport-web-client/src/app/agency/timetable/create-timetable/create-timetable.component.ts
+++ public-transport-web-client/src/app/agency/timetable/create-timetable/create-timetable.component.ts
@@ -1,5 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {TimetableBoardComponent} from "./timetable-board/timetable-board.component";
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
     selector: 'app-create-timetable',
@@ -10,6 +11,30 @@ import {TimetableBoardComponent} from "./timetable-board/timetable-board.compone
     templateUrl: './create-timetable.component.html',
     styleUrl: './create-timetable.component.scss'
 })
-export class CreateTimetableComponent {
+export class CreateTimetableComponent implements OnInit {
 
+    public formGroup: FormGroup;
+
+    constructor(private formBuilder: FormBuilder) {
+    }
+
+    ngOnInit(): void {
+        this.formGroup = this.formBuilder.group({
+            workingDay: this.buildTimetable(),
+            saturday: this.buildTimetable(),
+            sunday: this.buildTimetable()
+        })
+    }
+
+    private buildTimetable() {
+        return this.formBuilder.group({
+            startTime: [''],
+            endTime: [''],
+            interval: 5
+        });
+    }
+
+    public getTimetableByName(name: string): FormGroup {
+        return this.formGroup.get(name) as FormGroup;
+    }
 }
--- public-transport-web-client/src/app/agency/timetable/create-timetable/timetable-board/timetable-board.component.ts
+++ public-transport-web-client/src/app/agency/timetable/create-timetable/timetable-board/timetable-board.component.ts
@@ -1,6 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, forwardRef, Input, OnInit} from '@angular/core';
 import {CommonModule} from "@angular/common";
-import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {FormBuilder, FormGroup, FormsModule, NG_VALUE_ACCESSOR, ReactiveFormsModule, Validators} from "@angular/forms";
 import moment, {Moment} from "moment";
 import {NgxMaterialTimepickerModule} from "ngx-material-timepicker";
 import {faClock, faGlobe, faSpinner, IconDefinition} from "@fortawesome/free-solid-svg-icons";
@@ -10,13 +10,6 @@ export interface Departure {
     hour: number;
     minutes: number | null;
     symbol: string;
-}
-
-
-export interface TimetableGeneratorConfig {
-    startTimeGenerator: string;
-    endTimeGenerator: string;
-    intervalInMinutes: number;
 }
 
 @Component({
@@ -34,31 +27,21 @@ export interface TimetableGeneratorConfig {
 })
 export class TimetableBoardComponent implements OnInit {
 
+    @Input() group!: FormGroup;
+
     public faClock: IconDefinition = faClock;
     public faGlobe: IconDefinition = faGlobe;
     public faSpinner: IconDefinition = faSpinner;
 
     public hours: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
 
-    public generatorConfig: TimetableGeneratorConfig = {} as TimetableGeneratorConfig;
-
     public departures: Map<number, Departure[]> = new Map<number, Departure[]>();
 
-    public timetableGeneratorForm: FormGroup;
-
-    constructor(private formBuilder: FormBuilder) {
+    constructor() {
     }
 
-
     ngOnInit(): void {
-        this.timetableGeneratorForm = this.formBuilder.group({
-            startTimePicker: ['', [Validators.required, Validators.min(0)]],
-            endTimePicker: ['', [Validators.required, Validators.min(0)]],
-            interval: ['', [Validators.required, Validators.min(0)]]
-        });
-
         this.departures = this.mapDepartures([], true);
-
     }
 
     public mapDepartures(generatedDepartures: Moment[], appendEmpty: boolean = false): Map<number, Departure[]> {
@@ -126,13 +109,23 @@ export class TimetableBoardComponent implements OnInit {
     }
 
     public generateTimetable(modelForm: FormGroup): void {
-        const start: Moment = moment(modelForm.controls['startTimePicker'].value, 'HH:mm');
-        const end: Moment = moment(modelForm.controls['endTimePicker'].value, 'HH:mm');
+        const start: Moment = moment(modelForm.controls['startTime'].value, 'HH:mm');
+        const end: Moment = moment(modelForm.controls['endTime'].value, 'HH:mm');
         const intervalInMinutes: number = modelForm.controls['interval'].value;
 
         const times: Moment[] = this.timesBetween(start, end, intervalInMinutes);
 
+        console.log(start);
+        console.log(end);
+        console.log(intervalInMinutes);
+
         this.departures = this.mapDepartures(times, true);
     }
+
+    openPicker(input: HTMLInputElement, picker: any): void {
+        input.focus();
+        picker.open();
+    }
+
 
 }
