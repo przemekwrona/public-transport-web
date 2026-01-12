e20b8fe16bf8f82b6c1162c51212376ef7ea0134
przemekwrona <przemekwrona25@gmail.com>
2026-01-12 11:48:34 +0100

    feat: set calendars name on frontend

--- public-transport-web-server/src/main/java/pl/wrona/webserver/bussiness/timetable/generator/details/TimetableGeneratorDetailsService.java
+++ public-transport-web-server/src/main/java/pl/wrona/webserver/bussiness/timetable/generator/details/TimetableGeneratorDetailsService.java
@@ -5,7 +5,11 @@ import org.igeolab.iot.pt.server.api.model.GetTimetableGeneratorDetailsResponse;
 import org.igeolab.iot.pt.server.api.model.TimetableGeneratorId;
 import org.igeolab.iot.pt.server.api.model.TimetableGeneratorPayload;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import pl.wrona.webserver.bussiness.timetable.generator.TimetableGeneratorItemQueryService;
 import pl.wrona.webserver.bussiness.timetable.generator.TimetableGeneratorQueryService;
+import pl.wrona.webserver.core.calendar.CalendarEntity;
+import pl.wrona.webserver.core.timetable.TimetableGeneratorItemEntity;
 import pl.wrona.webserver.security.PreAgencyAuthorize;
 
 @Service
@@ -13,17 +17,21 @@ import pl.wrona.webserver.security.PreAgencyAuthorize;
 public class TimetableGeneratorDetailsService {
 
     private final TimetableGeneratorQueryService timetableGeneratorQueryService;
+    private final TimetableGeneratorItemQueryService timetableGeneratorItemQueryService;
 
+    @Transactional
     @PreAgencyAuthorize
     public GetTimetableGeneratorDetailsResponse findTimetableGeneratorDetails(String instance, TimetableGeneratorId timetableGeneratorId) {
         var timetable = timetableGeneratorQueryService.findByAgencyAndRouteIdAndCreateDate(instance, timetableGeneratorId.getRouteId(), timetableGeneratorId.getCreatedAt());
+        var items = timetableGeneratorItemQueryService.findAllByTimetableGenerator(timetable);
 
         return new GetTimetableGeneratorDetailsResponse()
                 .timetableGeneratorId(new TimetableGeneratorId()
                         .routeId(timetableGeneratorId.getRouteId())
                         .timetableVersion(timetableGeneratorId.getTimetableVersion())
                         .createdAt(timetableGeneratorId.getCreatedAt()))
+                .routeId(timetableGeneratorId.getRouteId())
                 .timetables(new TimetableGeneratorPayload()
-                        .calendarName(""));
+                        .calendarName(items.stream().findFirst().map(TimetableGeneratorItemEntity::getCalendar).map(CalendarEntity::getCalendarName).orElse("")));
     }
 }
