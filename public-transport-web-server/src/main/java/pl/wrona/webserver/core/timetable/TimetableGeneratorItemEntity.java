2e39f89d16a89fbfdda2920d9ea7fd86f318c6c5
przemekwrona <przemekwrona25@gmail.com>
2026-01-20 17:27:06 +0100

    feat: change primary key id

--- public-transport-web-server/src/main/java/pl/wrona/webserver/core/timetable/TimetableGeneratorItemEntity.java
+++ public-transport-web-server/src/main/java/pl/wrona/webserver/core/timetable/TimetableGeneratorItemEntity.java
@@ -2,12 +2,10 @@ package pl.wrona.webserver.core.timetable;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -26,13 +24,18 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "timetable_generator_item")
+@IdClass(TimetableGeneratorItemEntity.class)
 public class TimetableGeneratorItemEntity {
 
     @Id
-    @Column(name = "timetable_generator_item_id")
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "timetable_generator_item_id_seq")
-    @SequenceGenerator(name = "timetable_generator_item_id_seq", sequenceName = "timetable_generator_item_id_seq", allocationSize = 1)
-    private Long timetableGeneratorId;
+    @ManyToOne
+    @JoinColumn(name = "service_id", nullable = false)
+    private CalendarEntity calendar;
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "timetable_generator_id", nullable = false)
+    private TimetableGeneratorEntity timetableGenerator;
 
     @Column(name = "front_start_time")
     private LocalTime frontStartTime;
@@ -60,11 +63,4 @@ public class TimetableGeneratorItemEntity {
     @Column(name = "back_payload", columnDefinition = "jsonb")
     private List<TimetableDeparture> backPayload;
 
-    @ManyToOne
-    @JoinColumn(name = "service_id", nullable = false)
-    private CalendarEntity calendar;
-
-    @ManyToOne
-    @JoinColumn(name = "timetable_generator_id", nullable = false)
-    private TimetableGeneratorEntity timetableGenerator;
 }
