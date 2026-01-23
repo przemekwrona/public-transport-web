91bab66ebdf3fa3c5ee248d9bb51102526ad611b
przemekwrona <przemekwrona25@gmail.com>
2026-01-23 17:27:01 +0100

    feat: add angular material to calendar tab

--- public-transport-web-client/src/app/agency/calendars/calendars.module.ts
+++ public-transport-web-client/src/app/agency/calendars/calendars.module.ts
@@ -2,24 +2,49 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {CalendarsEditorComponent} from "./calendar-editor/calendars-editor.component";
 import {SharedModule} from "../shared/shared.module";
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {CalendarListComponent} from "./calendar-list/calendar-list.component";
 import {RouterModule} from "@angular/router";
 import {CalendarService} from "../../generated/public-transport-api";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {FaIconComponent} from "@fortawesome/angular-fontawesome";
+import {MatCheckbox} from "@angular/material/checkbox";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {provideLuxonDateAdapter} from "@angular/material-luxon-adapter";
+
+export const MY_FORMATS = {
+    parse: {
+        dateInput: 'dd-MM-yyyy',
+    },
+    display: {
+        dateInput: 'dd-MM-yyyy',
+        monthYearLabel: 'yyyy MMM',
+        dateA11yLabel: 'DD',
+        monthYearA11yLabel: 'yyyy MMMM'
+    },
+};
 
 @NgModule({
     imports: [
         CommonModule,
         RouterModule,
         SharedModule,
-        FormsModule
+        FormsModule,
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatCheckbox,
+        MatDatepickerModule,
+        FaIconComponent
     ],
     declarations: [
         CalendarListComponent,
         CalendarsEditorComponent
     ],
     providers: [
-        CalendarService
+        CalendarService,
+        provideLuxonDateAdapter(MY_FORMATS)
     ]
 })
 export class CalendarsModule {
