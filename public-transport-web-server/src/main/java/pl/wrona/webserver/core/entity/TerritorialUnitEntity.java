0e8edcea8e6067926adbddb20f3e615e40a15331
przemekwrona <przemekwrona25@gmail.com>
2026-04-04 16:05:02 +0200

    feat: find territories by stops

---
+++ public-transport-web-server/src/main/java/pl/wrona/webserver/core/entity/TerritorialUnitEntity.java
@@ -0,0 +1,65 @@
+package pl.wrona.webserver.core.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import pl.wrona.webserver.stops.BusStop;
+
+import java.util.Set;
+
+@Entity
+@Data
+@Builder
+@Table(name = "territorial_unit")
+@NoArgsConstructor
+@AllArgsConstructor
+public class TerritorialUnitEntity {
+
+    @Id
+    @Column(name = "territorial_unit_id")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "territorial_unit_id_seq")
+    @SequenceGenerator(name = "territorial_unit_id_seq", sequenceName = "territorial_unit_id_seq", allocationSize = 1)
+    private Long territorialUnitId;
+
+    @Column(name = "lokalnyid")
+    private String lokalnyid;
+
+    @Column(name = "teryt")
+    private String teryt;
+
+    @Column(name = "idterc")
+    private String idterc;
+
+    @Column(name = "idsimc")
+    private String idsimc;
+
+    @Column(name = "idprng")
+    private String idprng;
+
+    @Column(name = "nazwa")
+    private String nazwa;
+
+    @Column(name = "rodzaj")
+    private String rodzaj;
+
+    @Column(name = "licz_miesz")
+    private String liczMiesz;
+
+    @Column(name = "centroid_lon")
+    private String centroidLon;
+
+    @Column(name = "centroid_lat")
+    private String centroidLat;
+
+    @OneToMany(mappedBy = "territorialUnit")
+    private Set<BusStop> busStops;
+}
---
+++ public-transport-web-server/src/main/java/pl/wrona/webserver/core/entity/TerritorialUnitRepository.java
@@ -0,0 +1,15 @@
+package pl.wrona.webserver.core.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TerritorialUnitRepository extends JpaRepository<TerritorialUnitEntity, Long> {
+
+    @Query(value = "SELECT t FROM TerritorialUnitEntity t WHERE t.territorialUnitId IN :territoriesIds")
+    List<TerritorialUnitEntity> findAllByStopIn(@Param("territoriesIds") List<Long> territoriesIds);
+}
