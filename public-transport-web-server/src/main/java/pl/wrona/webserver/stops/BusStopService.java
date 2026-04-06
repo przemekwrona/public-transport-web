cbb4395ab8d69e981d7e6146a55c3d4878441d4a
przemekwrona <przemekwrona25@gmail.com>
2026-04-06 09:04:06 +0200

    feat: add teryt to model

--- public-transport-web-server/src/main/java/pl/wrona/webserver/stops/BusStopService.java
+++ public-transport-web-server/src/main/java/pl/wrona/webserver/stops/BusStopService.java
@@ -6,14 +6,19 @@ import org.igeolab.iot.pt.server.api.model.Status;
 import org.igeolab.iot.pt.server.api.model.StopsPatchRequest;
 import org.igeolab.iot.pt.server.api.model.StopsResponse;
 import org.igeolab.iot.pt.server.api.model.Territory;
+import org.igeolab.iot.pt.server.api.model.Teryt;
 import org.springframework.stereotype.Service;
 import pl.wrona.webserver.core.AgencyService;
 import pl.wrona.webserver.core.TerritorialUnitQueryService;
+import pl.wrona.webserver.core.TerytQueryService;
 import pl.wrona.webserver.core.agency.AgencyEntity;
 import pl.wrona.webserver.core.entity.TerritorialUnitEntity;
+import pl.wrona.webserver.core.entity.TerytEntity;
 
 import java.util.Comparator;
 import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -22,15 +27,33 @@ public class BusStopService {
     private final BusStopRepository busStopRepository;
     private final AgencyService agencyService;
     private final TerritorialUnitQueryService territorialUnitQueryService;
+    private final TerytQueryService terytQueryService;
 
     public StopsResponse findBusStop(float maxLat, float minLon, float minLat, float maxLon) {
         var stopEntities = busStopRepository.findAllByArea(maxLat, minLon, minLat, maxLon);
-        var stopIds = stopEntities.stream().map(BusStop::getTerritorialUnit).filter(Objects::nonNull).map(TerritorialUnitEntity::getTerritorialUnitId).toList();
-        var territoryEntities = territorialUnitQueryService.findAllByStopIn(stopIds);
+        var territorialUnitIds = stopEntities.stream().map(BusStop::getTerritorialUnit).filter(Objects::nonNull).map(TerritorialUnitEntity::getTerritorialUnitId).toList();
+        var territoryEntities = territorialUnitQueryService.findAllByStopIn(territorialUnitIds);
+        var tertitoryIdterc = territoryEntities.stream().map(TerritorialUnitEntity::getIdterc).collect(Collectors.toSet());
+        var tertitoryTeryts = territoryEntities.stream().map(TerritorialUnitEntity::getTeryt).collect(Collectors.toSet());
+
+        var gminy = terytQueryService.findTerrytGminaByIdtercIn(tertitoryIdterc);
+        var powiaty = terytQueryService.findTerrytPowiatByTeryIn(tertitoryTeryts);
+        var wojewodztwa = terytQueryService.findTerrytWojewodztwoByWojIn(tertitoryTeryts).stream()
+                .filter(Objects::nonNull)
+                .collect(Collectors.toMap(TerytEntity::getWoj, Function.identity()));
+
+        var teryts = powiaty.stream()
+                .map(it -> new Teryt()
+                        .teryt(it.getTeryt())
+                        .terytName(it.getNazwa())
+                        .voivodeshipTeryt(wojewodztwa.get(it.getWoj()).getTeryt())
+                        .voivodeshipName(wojewodztwa.get(it.getWoj()).getNazwa()))
+                .collect(Collectors.toMap(Teryt::getTeryt, Function.identity()));
+
         var territories = territoryEntities.stream()
                 .map(it -> new Territory()
                         .id(it.getLokalnyid())
-                        .teryt(it.getTeryt())
+                        .teryt(teryts.get(it.getTeryt()))
                         .idterc(it.getIdterc())
                         .name(it.getNazwa())
                         .type(it.getRodzaj()))
