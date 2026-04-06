88972c0717a8b7023bafe7a3376bfff7d7be44e9
przemekwrona <przemekwrona25@gmail.com>
2026-04-06 10:07:59 +0200

    feat: init city selector

---
+++ public-transport-web-client/src/app/agency/shared/city-selector/city-selector.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-city-selector',
+  imports: [],
+  templateUrl: './city-selector.component.html',
+  styleUrl: './city-selector.component.scss'
+})
+export class CitySelectorComponent {
+
+}
