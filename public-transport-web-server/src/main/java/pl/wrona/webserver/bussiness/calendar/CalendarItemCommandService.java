2709d56cbbe54e79cdf02023b05e038581f1b56b
przemekwrona <przemekwrona25@gmail.com>
2026-04-17 19:12:42 +0200

    feat: save calendar

--- public-transport-web-server/src/main/java/pl/wrona/webserver/bussiness/calendar/CalendarItemCommandService.java
+++ public-transport-web-server/src/main/java/pl/wrona/webserver/bussiness/calendar/CalendarItemCommandService.java
@@ -3,18 +3,18 @@ package pl.wrona.webserver.bussiness.calendar;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import pl.wrona.webserver.core.calendar.CalendarItemCommandRepository;
 import pl.wrona.webserver.core.calendar.CalendarItemEntity;
-import pl.wrona.webserver.core.calendar.CalendarItemQueryRepository;
 
 @Service
 @AllArgsConstructor
 public class CalendarItemCommandService {
 
-    private final CalendarItemQueryRepository calendarItemQueryRepository;
+    private final CalendarItemCommandRepository calendarItemCommandRepository;
 
     @Transactional
     public CalendarItemEntity save(CalendarItemEntity calendarItemEntity) {
-        return calendarItemQueryRepository.save(calendarItemEntity);
+        return calendarItemCommandRepository.save(calendarItemEntity);
     }
 
 }
