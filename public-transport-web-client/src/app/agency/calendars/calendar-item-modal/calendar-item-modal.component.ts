2e051a975768d18337564e67ee4e26cbd552f5e8
przemekwrona <przemekwrona25@gmail.com>
2026-05-10 00:00:26 +0200

    feat: select date range

--- public-transport-web-client/src/app/agency/calendars/calendar-item-modal/calendar-item-modal.component.ts
+++ public-transport-web-client/src/app/agency/calendars/calendar-item-modal/calendar-item-modal.component.ts
@@ -1,16 +1,53 @@
 import {Component} from '@angular/core';
-import {MatDialogModule} from "@angular/material/dialog";
+import {MatDialogModule, MatDialogRef} from "@angular/material/dialog";
 import {MatButtonModule} from "@angular/material/button";
+import {FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {SharedModule} from "../../shared/shared.module";
+import {CommonModule} from "@angular/common";
 
 @Component({
     selector: 'app-calendar-item-modal',
     imports: [
+        CommonModule,
         MatDialogModule,
-        MatButtonModule
+        MatButtonModule,
+        MatFormFieldModule,
+        MatDatepickerModule,
+        FormsModule,
+        ReactiveFormsModule,
+        SharedModule
     ],
     templateUrl: './calendar-item-modal.component.html',
     styleUrl: './calendar-item-modal.component.scss'
 })
 export class CalendarItemModalComponent {
+
+    public modelForm: FormGroup;
+    public isSubmitted: boolean = false;
+
+    get startDateControl(): FormControl {
+        return this.modelForm.get('startDate') as FormControl;
+    }
+
+    get endDateControl(): FormControl {
+        return this.modelForm.get('endDate') as FormControl;
+    }
+
+    constructor(private formBuilder: FormBuilder, private dialogRef: MatDialogRef<CalendarItemModalComponent>) {
+        this.modelForm = this.formBuilder.group({
+            startDate: ['', [Validators.required]],
+            endDate: ['', [Validators.required]]
+        });
+    }
+
+    public createCalendarItem(): void {
+        this.isSubmitted = true;
+
+        if (this.modelForm.valid) {
+            this.dialogRef.close();
+        }
+    }
 
 }
