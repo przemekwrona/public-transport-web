8b95a17c42b859985f176928aae91f12bc4c48d3
przemekwrona <przemekwrona25@gmail.com>
2026-08-06 08:47:35 +0200

    feat: init on time range selected modal

---
+++ public-transport-web-client/src/app/agency/brigade/brigade-scheduler/on-time-range-selected-modal/on-time-range-selected-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-on-time-range-selected-modal',
+  imports: [],
+  templateUrl: './on-time-range-selected-modal.component.html',
+  styleUrl: './on-time-range-selected-modal.component.scss'
+})
+export class OnTimeRangeSelectedModalComponent {
+
+}
