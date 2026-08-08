e4ba35cdc7dc8717ba3a7408d3148352e06c1b95
przemekwrona <przemekwrona25@gmail.com>
2026-08-08 14:33:11 +0200

    feat: return data from modal

--- public-transport-web-client/src/app/agency/brigade/brigade-scheduler/on-time-range-selected-modal/on-time-range-selected-modal.component.ts
+++ public-transport-web-client/src/app/agency/brigade/brigade-scheduler/on-time-range-selected-modal/on-time-range-selected-modal.component.ts
@@ -1,11 +1,69 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {TripVariantSelectComponent} from "../trip-variant-select/trip-variant-select.component";
+import {
+    MAT_DIALOG_DATA,
+    MatDialogActions,
+    MatDialogClose,
+    MatDialogContent, MatDialogRef,
+    MatDialogTitle
+} from "@angular/material/dialog";
+import {TripId, TripId1, TripService} from "../../../../generated/public-transport-api";
+import {CommonModule} from "@angular/common";
+import {FormBuilder, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {MatButton} from "@angular/material/button";
+import {AgencyStorageService} from "../../../../auth/agency-storage.service";
+import moment from "moment";
+
+export interface OnTimeRangeAndTripSelected {
+    start: string
+    end: string
+    tripId: TripId1
+    origin: string
+    destination: string
+}
 
 @Component({
-  selector: 'app-on-time-range-selected-modal',
-  imports: [],
-  templateUrl: './on-time-range-selected-modal.component.html',
-  styleUrl: './on-time-range-selected-modal.component.scss'
+    selector: 'app-on-time-range-selected-modal',
+    imports: [
+        CommonModule,
+        TripVariantSelectComponent,
+        MatDialogContent,
+        MatDialogTitle,
+        FormsModule,
+        MatButton,
+        MatDialogActions,
+        MatDialogClose,
+        ReactiveFormsModule
+    ],
+    templateUrl: './on-time-range-selected-modal.component.html',
+    styleUrl: './on-time-range-selected-modal.component.scss'
 })
 export class OnTimeRangeSelectedModalComponent {
+
+    private formBuilder: FormBuilder = inject(FormBuilder);
+
+    data = inject<{ start: string, end: string }>(MAT_DIALOG_DATA);
+
+    modelForm = this.formBuilder.group({
+        route: [null, [Validators.required]]
+    });
+
+    constructor(private dialogRef: MatDialogRef<OnTimeRangeSelectedModalComponent>, private agencyStorageService: AgencyStorageService, private tripService: TripService) {
+    }
+
+    public selectTrip() {
+        const instance = this.agencyStorageService.getInstance();
+        const tripId: TripId = this.modelForm.controls['route'].value;
+        this.tripService.getTripByVariant(instance, tripId).subscribe(tripDetails => {
+            const results: OnTimeRangeAndTripSelected = {} as OnTimeRangeAndTripSelected;
+            results.start = this.data.start;
+            results.end = moment(this.data.start).add(1 * 60 * 60, 'seconds').format("yyyy-MM-DDTHH:mm:SS");
+            results.tripId = tripId;
+            results.origin = tripDetails.originStopName;
+            results.destination = tripDetails.destinationStopName;
+
+            this.dialogRef.close(results);
+        });
+    }
 
 }
