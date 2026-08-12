b5f5b3265e4af6e9d62057e1a0e97d37fd5213b9
przemekwrona <przemekwrona25@gmail.com>
2026-08-12 21:02:42 +0200

    feat: create sequencer entity

---
+++ public-transport-web-server/src/main/java/pl/wrona/webserver/core/calendar/CalendarSequenceEntity.java
@@ -0,0 +1,25 @@
+package pl.wrona.webserver.core.calendar;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "calendar_sequences")
+public class CalendarSequenceEntity {
+
+    @Id
+    @Column(name = "agency_code", length = 15, nullable = false)
+    private String agencyCode;
+
+    @Column(name = "next_value", nullable = false)
+    private Integer nextValue;
+
+}
