34e062be884671660e47edde87ae3484273c81be
przemekwrona <przemekwrona25@gmail.com>
2026-08-14 20:55:29 +0200

    feat: resolve query

--- public-transport-web-client/src/app/agency/brigade/brigade-editor/brigade.resolver.ts
+++ public-transport-web-client/src/app/agency/brigade/brigade-editor/brigade.resolver.ts
@@ -1,6 +1,6 @@
 import {ResolveFn} from '@angular/router';
 import {Observable} from "rxjs";
-import {BrigadeBody, BrigadePayload, BrigadeService} from "../../../generated/public-transport-api";
+import {BrigadeBody, BrigadePayload, BrigadeService, CalendarSymbolId} from "../../../generated/public-transport-api";
 import {inject} from "@angular/core";
 import {AgencyStorageService} from "../../../auth/agency-storage.service";
 
@@ -10,8 +10,15 @@ export const brigadeResolver: ResolveFn<Observable<BrigadeBody>> = (route, state
 
     const instance: string = agencyStorageService.getInstance();
     const brigadeName: string = route.queryParams['name'];
+    const calendarCode: string = route.paramMap.get('calendarCode');
+    const calendarSymbol: string = route.paramMap.get('calendarSymbol');
+
+    const calendarSymbolId: CalendarSymbolId = {};
+    calendarSymbolId.calendarItemId = {code: calendarCode};
+    calendarSymbolId.symbol = calendarSymbol;
 
     const brigadePayload: BrigadePayload = {};
+    brigadePayload.calendarSymbolId = calendarSymbolId;
     brigadePayload.brigadeName = brigadeName;
 
     return brigadeService.getBrigadeByBrigadeName(instance, brigadePayload);
