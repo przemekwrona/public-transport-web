4bf88869630e95c802b5cdcd5dd9130bc767ff16
przemekwrona <przemekwrona25@gmail.com>
2026-08-14 23:45:18 +0200

    feat: introduce BrigadeEventQueryService and enhance event handling in updater and scheduler components

--- public-transport-web-server/src/main/java/pl/wrona/webserver/core/brigade/BrigadeEventQueryRepository.java
+++ public-transport-web-server/src/main/java/pl/wrona/webserver/core/brigade/BrigadeEventQueryRepository.java
@@ -1,6 +1,8 @@
 package pl.wrona.webserver.core.brigade;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
@@ -10,4 +12,16 @@ import java.util.List;
 public interface BrigadeEventQueryRepository extends JpaRepository<BrigadeEventEntity, Long> {
 
     List<BrigadeEventEntity> findAllByResourceBrigadeResourceIdInOrderByStartSecondAsc(Collection<Long> resourceIds);
+
+    @Query("""
+            SELECT e FROM BrigadeEventEntity e
+            WHERE e.resource.brigadeGroup.calendarSymbol.calendarItem.agency.agencyCode = :agency
+            AND e.resource.brigadeGroup.calendarSymbol.calendarItem.sequenceHex = :calendarCode
+            AND e.resource.brigadeGroup.calendarSymbol.designation = :symbol
+            AND e.sequenceHex = :eventCode""")
+    BrigadeEventEntity findByAgencyAndCalendarAndSymbolAndEventHex(
+            @Param("agency") String agency,
+            @Param("calendarCode") String calendarCode,
+            @Param("symbol") String symbol,
+            @Param("eventCode") String eventCode);
 }
