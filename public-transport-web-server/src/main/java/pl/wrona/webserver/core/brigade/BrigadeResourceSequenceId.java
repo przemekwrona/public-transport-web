c93bbb3a488294cc787719ea5cbc8ca18f018e74
przemekwrona <przemekwrona25@gmail.com>
2026-08-15 08:32:20 +0200

    feat: add brigade_item table and brigade_item_sequence table for enhanced brigade resource and event sequence handling

--- public-transport-web-server/src/main/java/pl/wrona/webserver/core/brigade/BrigadeResourceSequenceId.java
+++ public-transport-web-server/src/main/java/pl/wrona/webserver/core/brigade/BrigadeResourceSequenceId.java
@@ -17,6 +17,9 @@ public class BrigadeResourceSequenceId implements Serializable {
     @Column(name = "agency_code", length = 15, nullable = false)
     private String agencyCode;
 
+    @Column(name = "brigade_item_sequence", nullable = false)
+    private Integer brigadeItemSequence;
+
     @Column(name = "calendar_item_sequence", nullable = false)
     private Integer calendarItemSequence;
 
