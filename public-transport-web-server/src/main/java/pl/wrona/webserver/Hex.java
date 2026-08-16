54ec2341c8002354a9fbc3585441764b0fd23ba0
przemekwrona <przemekwrona25@gmail.com>
2026-08-16 22:19:02 +0200

    feat: add support for 5-character base-36 conversion in Hex utility class

--- public-transport-web-server/src/main/java/pl/wrona/webserver/Hex.java
+++ public-transport-web-server/src/main/java/pl/wrona/webserver/Hex.java
@@ -6,9 +6,11 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class Hex {
 
+    public static final int RADIX_36 = 36;
+
     public String toHex(Integer sequence) {
         // Converts an Integer to a zero-padded 4-character base-36 string
-        return String.format("%4s", Integer.toString(sequence, 36)).replace(' ', '0');
+        return String.format("%4s", Integer.toString(sequence, RADIX_36)).replace(' ', '0');
     }
 
     public Integer fromHex(String hex) {
@@ -16,7 +18,12 @@ public class Hex {
         if (hex == null) {
             return null;
         }
-        return Integer.parseInt(hex, 36);
+        return Integer.parseInt(hex, RADIX_36);
+    }
+
+    public String toHex5(Integer sequence) {
+        // Converts an Integer to a zero-padded 5-character base-36 string
+        return String.format("%5s", Integer.toString(sequence, RADIX_36)).replace(' ', '0');
     }
 
 }
