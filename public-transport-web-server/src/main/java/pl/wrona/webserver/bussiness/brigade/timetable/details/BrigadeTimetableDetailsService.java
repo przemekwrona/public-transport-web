981ced0bb3ce96a2aa9695f48d685f2b02335679
przemekwrona <przemekwrona25@gmail.com>
2026-08-16 14:09:05 +0200

    feat: add query for fetching brigade events by agency, brigade, calendar, and symbol in timetable service

--- public-transport-web-server/src/main/java/pl/wrona/webserver/bussiness/brigade/timetable/details/BrigadeTimetableDetailsService.java
+++ public-transport-web-server/src/main/java/pl/wrona/webserver/bussiness/brigade/timetable/details/BrigadeTimetableDetailsService.java
@@ -3,12 +3,24 @@ package pl.wrona.webserver.bussiness.brigade.timetable.details;
 import lombok.AllArgsConstructor;
 import org.igeolab.iot.pt.server.api.model.GetTimetableByBrigadeResponse;
 import org.springframework.stereotype.Service;
+import pl.wrona.webserver.bussiness.brigade.event.BrigadeEventQueryService;
+import pl.wrona.webserver.core.brigade.BrigadeEventEntity;
+import pl.wrona.webserver.security.PreAgencyAuthorize;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class BrigadeTimetableDetailsService {
 
-    public GetTimetableByBrigadeResponse getTimetableByBrigadeAndCalendarSymbol(String instance, String brigadeCode, String calendarCode, String calendarSymbol) {
-        return null;
+    private final BrigadeEventQueryService brigadeEventQueryService;
+
+    @PreAgencyAuthorize
+    public GetTimetableByBrigadeResponse getTimetableByBrigadeAndCalendarSymbol(
+            String instance, String brigadeCode, String calendarCode, String calendarSymbol) {
+        List<BrigadeEventEntity> events = brigadeEventQueryService.findAllByAgencyAndBrigadeAndCalendarAndSymbol(
+                instance, brigadeCode, calendarCode, calendarSymbol);
+
+        return new GetTimetableByBrigadeResponse();
     }
 }
--- public-transport-web-server/src/main/java/pl/wrona/webserver/core/brigade/BrigadeEventQueryRepository.java
+++ public-transport-web-server/src/main/java/pl/wrona/webserver/core/brigade/BrigadeEventQueryRepository.java
@@ -16,6 +16,19 @@ public interface BrigadeEventQueryRepository extends JpaRepository<BrigadeEventE
     @Query("""
             SELECT e FROM BrigadeEventEntity e
             WHERE e.resource.brigadeGroup.calendarSymbol.calendarItem.agency.agencyCode = :agency
+            AND e.resource.brigadeGroup.brigadeItem.sequenceHex = :brigadeCode
+            AND e.resource.brigadeGroup.calendarSymbol.calendarItem.sequenceHex = :calendarCode
+            AND e.resource.brigadeGroup.calendarSymbol.designation = :calendarSymbol
+            ORDER BY e.startSecond ASC""")
+    List<BrigadeEventEntity> findAllByAgencyAndBrigadeAndCalendarAndSymbol(
+            @Param("agency") String agency,
+            @Param("brigadeCode") String brigadeCode,
+            @Param("calendarCode") String calendarCode,
+            @Param("calendarSymbol") String calendarSymbol);
+
+    @Query("""
+            SELECT e FROM BrigadeEventEntity e
+            WHERE e.resource.brigadeGroup.calendarSymbol.calendarItem.agency.agencyCode = :agency
             AND e.resource.brigadeGroup.calendarSymbol.calendarItem.sequenceHex = :calendarCode
             AND e.resource.brigadeGroup.calendarSymbol.designation = :symbol
             AND e.sequenceHex = :eventCode""")
