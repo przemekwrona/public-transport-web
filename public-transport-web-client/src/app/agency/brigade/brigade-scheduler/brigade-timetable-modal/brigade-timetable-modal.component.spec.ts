bd0fdaf34351562749776ceba8f6f44410f36db1
przemekwrona <przemekwrona25@gmail.com>
2026-08-16 15:45:32 +0200

    feat: integrate timetable data fetching and display in BrigadeTimetableModal component

--- public-transport-web-client/src/app/agency/brigade/brigade-scheduler/brigade-timetable-modal/brigade-timetable-modal.component.spec.ts
+++ public-transport-web-client/src/app/agency/brigade/brigade-scheduler/brigade-timetable-modal/brigade-timetable-modal.component.spec.ts
@@ -1,23 +1,33 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { BrigadeTimetableModalComponent } from './brigade-timetable-modal.component';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {of} from 'rxjs';
+import {BrigadeTimetableService} from '../../../../generated/public-transport-api';
+import {AgencyStorageService} from '../../../../auth/agency-storage.service';
+import {BrigadeTimetableModalComponent} from './brigade-timetable-modal.component';
 
 describe('BrigadeTimetableModalComponent', () => {
-  let component: BrigadeTimetableModalComponent;
-  let fixture: ComponentFixture<BrigadeTimetableModalComponent>;
+    let component: BrigadeTimetableModalComponent;
+    let fixture: ComponentFixture<BrigadeTimetableModalComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [BrigadeTimetableModalComponent]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [BrigadeTimetableModalComponent],
+            providers: [
+                {provide: MAT_DIALOG_DATA, useValue: {brigadeCode: '01', calendarCode: '0001', calendarSymbol: 'A'}},
+                {provide: AgencyStorageService, useValue: {getInstance: () => 'test'}},
+                {
+                    provide: BrigadeTimetableService,
+                    useValue: {getTimetableByBrigadeAndCalendarSymbol: () => of({trips: []})}
+                }
+            ]
+        }).compileComponents();
 
-    fixture = TestBed.createComponent(BrigadeTimetableModalComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(BrigadeTimetableModalComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- public-transport-web-client/src/app/agency/brigade/brigade-scheduler/brigade-timetable-modal/brigade-timetable-modal.component.ts
+++ public-transport-web-client/src/app/agency/brigade/brigade-scheduler/brigade-timetable-modal/brigade-timetable-modal.component.ts
@@ -1,11 +1,62 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {
+    MAT_DIALOG_DATA,
+    MatDialogActions,
+    MatDialogClose,
+    MatDialogContent,
+    MatDialogTitle
+} from '@angular/material/dialog';
+import {MatButton} from '@angular/material/button';
+import {
+    BrigadeTimetableService,
+    GetTimetableByBrigadeResponse
+} from '../../../../generated/public-transport-api';
+import {AgencyStorageService} from '../../../../auth/agency-storage.service';
+
+export interface BrigadeTimetableModalData {
+    brigadeCode: string;
+    calendarCode: string;
+    calendarSymbol: string;
+}
 
 @Component({
-  selector: 'app-brigade-timetable-modal',
-  imports: [],
-  templateUrl: './brigade-timetable-modal.component.html',
-  styleUrl: './brigade-timetable-modal.component.scss'
+    selector: 'app-brigade-timetable-modal',
+    imports: [
+        CommonModule,
+        MatDialogTitle,
+        MatDialogContent,
+        MatDialogActions,
+        MatDialogClose,
+        MatButton
+    ],
+    providers: [
+        AgencyStorageService,
+        BrigadeTimetableService
+    ],
+    templateUrl: './brigade-timetable-modal.component.html',
+    styleUrl: './brigade-timetable-modal.component.scss'
 })
-export class BrigadeTimetableModalComponent {
+export class BrigadeTimetableModalComponent implements OnInit {
+
+    readonly data = inject<BrigadeTimetableModalData>(MAT_DIALOG_DATA);
+
+    timetable: GetTimetableByBrigadeResponse = {};
+
+    constructor(
+        private agencyStorageService: AgencyStorageService,
+        private brigadeTimetableService: BrigadeTimetableService) {
+    }
+
+    ngOnInit(): void {
+        const instance = this.agencyStorageService.getInstance();
+
+        this.brigadeTimetableService.getTimetableByBrigadeAndCalendarSymbol(
+            instance,
+            this.data.brigadeCode,
+            this.data.calendarCode,
+            this.data.calendarSymbol
+        ).subscribe(response => this.timetable = response ?? {});
+    }
 
 }
