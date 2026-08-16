9f6df008587d573fc7572af32542746d224b89e8
przemekwrona <przemekwrona25@gmail.com>
2026-08-16 15:18:00 +0200

    feat: integrate trip query in BrigadeEventUpdaterService to fetch trips by tripId

--- public-transport-web-server/src/main/java/pl/wrona/webserver/bussiness/brigade/event/updater/BrigadeEventUpdaterService.java
+++ public-transport-web-server/src/main/java/pl/wrona/webserver/bussiness/brigade/event/updater/BrigadeEventUpdaterService.java
@@ -4,10 +4,12 @@ import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.igeolab.iot.pt.server.api.model.PutBrigadeEventBody;
 import org.igeolab.iot.pt.server.api.model.Status;
+import org.igeolab.iot.pt.server.api.model.TripId;
 import org.springframework.stereotype.Service;
 import pl.wrona.webserver.bussiness.brigade.event.BrigadeEventQueryService;
 import pl.wrona.webserver.bussiness.brigade.resource.BrigadeResourceQueryService;
-import pl.wrona.webserver.bussiness.trip.TripCommandService;
+import pl.wrona.webserver.bussiness.trip.TripQueryService;
+import pl.wrona.webserver.core.agency.TripEntity;
 import pl.wrona.webserver.core.brigade.BrigadeEventCommandRepository;
 import pl.wrona.webserver.core.brigade.BrigadeEventEntity;
 import pl.wrona.webserver.security.PreAgencyAuthorize;
@@ -19,12 +21,20 @@ public class BrigadeEventUpdaterService {
     private final BrigadeEventCommandRepository brigadeEventCommandRepository;
     private final BrigadeEventQueryService brigadeEventQueryService;
     private final BrigadeResourceQueryService brigadeResourceQueryService;
+    private final TripQueryService tripQueryService;
 
     @PreAgencyAuthorize
     @Transactional
     public Status putBrigadeEvent(String instance, String calendarCode, String symbol, String resourceCode, PutBrigadeEventBody putBrigadeEventBody) {
         var resource = brigadeResourceQueryService.findByAgencyAndCalendarAndSymbolAndResourceCode(
                 instance, calendarCode, symbol, resourceCode);
+
+        var tripIdBody = putBrigadeEventBody.getTripId();
+        TripEntity trip = tripQueryService.findByAgencyCodeAndTripId(instance, new TripId()
+                .routeId(tripIdBody.getRouteId())
+                .variantName(tripIdBody.getVariantName())
+                .variantMode(tripIdBody.getVariantMode())
+                .trafficMode(tripIdBody.getTrafficMode()));
 
         var brigadeEvent = brigadeEventQueryService.findByAgencyAndCalendarAndSymbolAndEventHex(
                 instance, calendarCode, symbol, putBrigadeEventBody.getSequenceHex());
