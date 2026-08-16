53c49e3e66dc0139e4fcf34a7d8dae3951c0adfb
przemekwrona <przemekwrona25@gmail.com>
2026-08-16 16:22:11 +0200

    feat: integrate TimetableBoardComponent in BrigadeTimetableModal for detailed timetable display

--- public-transport-web-client/src/app/agency/brigade/brigade-scheduler/brigade-timetable-modal/timetable-board/timetable-board.component.ts
+++ public-transport-web-client/src/app/agency/brigade/brigade-scheduler/brigade-timetable-modal/timetable-board/timetable-board.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {
+    BrigadeTimetableDeparture,
+    BrigadeTimetableVariant
+} from '../../../../../generated/public-transport-api';
 
 @Component({
-  selector: 'app-timetable-board',
-  imports: [],
-  templateUrl: './timetable-board.component.html',
-  styleUrl: './timetable-board.component.scss'
+    selector: 'app-timetable-board',
+    imports: [
+        CommonModule
+    ],
+    templateUrl: './timetable-board.component.html',
+    styleUrl: './timetable-board.component.scss'
 })
 export class TimetableBoardComponent {
+
+    @Input() title: string = '';
+    @Input() variant: BrigadeTimetableVariant = {};
+
+    readonly hours: number[] = Array.from({length: 24}, (_, hour) => hour);
+
+    departuresForHour(hour: number): BrigadeTimetableDeparture[] {
+        return (this.variant?.departures ?? []).filter(departure => this.hourOf(departure) === hour);
+    }
+
+    private hourOf(departure: BrigadeTimetableDeparture): number | null {
+        if (departure.h != null) {
+            return departure.h;
+        }
+        if (departure.time) {
+            const [hours] = departure.time.split(':').map(Number);
+            return Number.isFinite(hours) ? hours : null;
+        }
+        return null;
+    }
 
 }
