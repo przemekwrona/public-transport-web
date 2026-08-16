f4755348638c6fc1c645e72a55e13944d8920fa3
przemekwrona <przemekwrona25@gmail.com>
2026-08-16 16:17:22 +0200

    feat: add TimetableBoardComponent with initial structure and tests

---
+++ public-transport-web-client/src/app/agency/brigade/brigade-scheduler/brigade-timetable-modal/timetable-board/timetable-board.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-timetable-board',
+  imports: [],
+  templateUrl: './timetable-board.component.html',
+  styleUrl: './timetable-board.component.scss'
+})
+export class TimetableBoardComponent {
+
+}
