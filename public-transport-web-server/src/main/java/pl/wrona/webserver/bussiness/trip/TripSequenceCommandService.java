a82d6dea916e029f3014a8042b726d2fc9555ac3
przemekwrona <przemekwrona25@gmail.com>
2026-08-16 22:42:19 +0200

    feat: add TripSequence services for managing trip sequences and integrate them into TripCreatorService

---
+++ public-transport-web-server/src/main/java/pl/wrona/webserver/bussiness/trip/TripSequenceCommandService.java
@@ -0,0 +1,28 @@
+package pl.wrona.webserver.bussiness.trip;
+
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import pl.wrona.webserver.core.agency.TripSequenceCommandRepository;
+import pl.wrona.webserver.core.agency.TripSequenceEntity;
+import pl.wrona.webserver.core.agency.TripSequenceId;
+import pl.wrona.webserver.core.agency.TripSequenceQueryRepository;
+
+@Service
+@AllArgsConstructor
+public class TripSequenceCommandService {
+
+    private final TripSequenceCommandRepository tripSequenceCommandRepository;
+    private final TripSequenceQueryRepository tripSequenceQueryRepository;
+
+    @Transactional
+    public TripSequenceEntity init(String agencyCode, Integer routeSequence) {
+        var existing = tripSequenceQueryRepository.findByAgencyCodeAndRouteSequence(agencyCode, routeSequence);
+        if (existing != null) {
+            return existing;
+        }
+        return tripSequenceCommandRepository.save(new TripSequenceEntity(
+                new TripSequenceId(agencyCode, routeSequence), 1L));
+    }
+
+}
