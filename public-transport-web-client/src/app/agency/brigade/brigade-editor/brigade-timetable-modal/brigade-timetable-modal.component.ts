ddfb799ca1396c4538937d10b6377679e0d035fa
przemekwrona <przemekwrona25@gmail.com>
2026-08-16 15:37:44 +0200

    feat: add BrigadeTimetableModalComponent with initial structure and tests

---
+++ public-transport-web-client/src/app/agency/brigade/brigade-editor/brigade-timetable-modal/brigade-timetable-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-brigade-timetable-modal',
+  imports: [],
+  templateUrl: './brigade-timetable-modal.component.html',
+  styleUrl: './brigade-timetable-modal.component.scss'
+})
+export class BrigadeTimetableModalComponent {
+
+}
