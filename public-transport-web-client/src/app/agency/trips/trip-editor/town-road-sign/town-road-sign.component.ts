be765d28eb2ca44a6c75d4b08c4bbfccac03215d
przemekwrona <przemekwrona25@gmail.com>
2026-08-21 23:34:40 +0200

    feat: add `TownRoadSignComponent` to trip editor with basic structure and unit tests

---
+++ public-transport-web-client/src/app/agency/trips/trip-editor/town-road-sign/town-road-sign.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-town-road-sign',
+  imports: [],
+  templateUrl: './town-road-sign.component.html',
+  styleUrl: './town-road-sign.component.scss'
+})
+export class TownRoadSignComponent {
+
+}
