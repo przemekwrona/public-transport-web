9682e246d0ff42c127ccbb86e5c9ef073e1f9135
przemekwrona <przemekwrona25@gmail.com>
2026-09-02 17:09:50 +0200

    feat: move default route from `BrigadeGroup` to `BrigadeItem`, update API and UI logic for brigade creation

--- public-transport-web-server/src/main/java/pl/wrona/webserver/bussiness/brigade/item/creator/BrigadeItemCreatorService.java
+++ public-transport-web-server/src/main/java/pl/wrona/webserver/bussiness/brigade/item/creator/BrigadeItemCreatorService.java
@@ -2,6 +2,8 @@ package pl.wrona.webserver.bussiness.brigade.item.creator;
 
 import lombok.AllArgsConstructor;
 import org.igeolab.iot.pt.server.api.model.BrigadeBody;
+import org.igeolab.iot.pt.server.api.model.CreateBrigadeBody;
+import org.igeolab.iot.pt.server.api.model.RouteId;
 import org.igeolab.iot.pt.server.api.model.Status;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,7 +11,10 @@ import pl.wrona.webserver.Hex;
 import pl.wrona.webserver.bussiness.brigade.item.BrigadeItemCommandService;
 import pl.wrona.webserver.bussiness.brigade.item.BrigadeItemSequenceQueryService;
 import pl.wrona.webserver.bussiness.calendar.CalendarItemQueryService;
+import pl.wrona.webserver.bussiness.route.RouteQueryService;
 import pl.wrona.webserver.core.AgencyService;
+import pl.wrona.webserver.core.agency.RouteEntity;
+import pl.wrona.webserver.core.brigade.BrigadeGroupEntity;
 import pl.wrona.webserver.core.brigade.BrigadeItemEntity;
 import pl.wrona.webserver.security.PreAgencyAuthorize;
 
@@ -21,19 +26,25 @@ public class BrigadeItemCreatorService {
     private final CalendarItemQueryService calendarItemQueryService;
     private final BrigadeItemSequenceQueryService brigadeItemSequenceQueryService;
     private final BrigadeItemCommandService brigadeItemCommandService;
+    private final RouteQueryService routeQueryService;
 
     @PreAgencyAuthorize
     @Transactional
-    public Status createBrigade(String instance, BrigadeBody request) {
+    public Status createBrigade(String instance, CreateBrigadeBody request) {
 
         var agencyEntity = agencyService.findAgencyByAgencyCode(instance);
-        var calendarCode = request.getCalendarSymbolId().getCalendarItemId().getCode();
+
+        var routeCode = request.getSelectedRouteCode();
+        var routeEntity = routeQueryService.findRouteByAgencyCodeAndRouteCode(agencyEntity.getAgencyCode(), routeCode);
+
+        var calendarCode = request.getCalendarCode();
         var calendarItem = calendarItemQueryService.findByAgencyCalendarCode(instance, calendarCode);
 
         var brigadeItemEntity = new BrigadeItemEntity();
         brigadeItemEntity.setName(request.getBrigadeName());
         brigadeItemEntity.setAgency(agencyEntity);
         brigadeItemEntity.setCalendarItem(calendarItem);
+        brigadeItemEntity.setDefaultRoute(routeEntity);
 
         var nextSequence = brigadeItemSequenceQueryService.findNextValue(instance, calendarCode);
         brigadeItemEntity.setSequence(nextSequence);
