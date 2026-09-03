7bb9dc0067aaf2677c29c1202e597806ea51304c
przemekwrona <przemekwrona25@gmail.com>
2026-09-04 00:24:25 +0200

    refactor: remove PDF trip generation module and all related classes, repositories, and services

--- public-transport-web-server/src/main/java/pl/wrona/webserver/bussiness/pdf/trip/PdfDeparture.java
+++
@@ -1,9 +0,0 @@
-package pl.wrona.webserver.bussiness.pdf.trip;
-
-public record PdfDeparture(
-        Long stopId,
-        String stopName,
-        int stopSequence,
-        String designation,
-        String departureTime) {
-}
