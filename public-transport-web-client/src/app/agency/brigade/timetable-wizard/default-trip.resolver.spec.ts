812458ed481b92592c06b2fce537f1dc105857fa
przemekwrona <przemekwrona25@gmail.com>
2026-09-05 18:59:18 +0200

    feat: enhance defaultTripResolver to return all trips for brigade default route, update tests and dependencies

--- public-transport-web-client/src/app/agency/brigade/timetable-wizard/default-trip.resolver.spec.ts
+++ public-transport-web-client/src/app/agency/brigade/timetable-wizard/default-trip.resolver.spec.ts
@@ -1,19 +1,67 @@
 import {TestBed} from '@angular/core/testing';
-import {ResolveFn} from '@angular/router';
-import {Observable} from 'rxjs';
+import {ActivatedRouteSnapshot, ResolveFn, RouterStateSnapshot} from '@angular/router';
+import {Observable, of} from 'rxjs';
 
 import {defaultTripResolver} from './default-trip.resolver';
-import {TripsDetails} from '../../../generated/public-transport-api';
+import {
+    BrigadeService,
+    GetAllTripsResponse,
+    TripService
+} from '../../../generated/public-transport-api';
+import {AgencyStorageService} from '../../../auth/agency-storage.service';
 
 describe('defaultTripResolver', () => {
-    const executeResolver: ResolveFn<Observable<TripsDetails>> = (...resolverParameters) =>
+    const executeResolver: ResolveFn<Observable<GetAllTripsResponse>> = (...resolverParameters) =>
         TestBed.runInInjectionContext(() => defaultTripResolver(...resolverParameters));
 
+    const tripsResponse: GetAllTripsResponse = {
+        lines: [{
+            trips: [
+                {isMainVariant: true, tripId: {tripCode: 'T1'}},
+                {isMainVariant: false, tripId: {tripCode: 'T2'}}
+            ]
+        }]
+    };
+
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        TestBed.configureTestingModule({
+            providers: [
+                {provide: AgencyStorageService, useValue: {getInstance: () => 'test-agency'}},
+                {
+                    provide: BrigadeService,
+                    useValue: {
+                        getBrigadeDetails: () => of({
+                            brigade: {defaultRouteCode: 'R1'}
+                        })
+                    }
+                },
+                {
+                    provide: TripService,
+                    useValue: {
+                        getTripsByRouteAndFilterLineOrName: jasmine.createSpy('getTripsByRouteAndFilterLineOrName')
+                            .and.returnValue(of(tripsResponse))
+                    }
+                }
+            ]
+        });
     });
 
     it('should be created', () => {
         expect(executeResolver).toBeTruthy();
+    });
+
+    it('should return all trips for the brigade default route', (done) => {
+        const route = {
+            paramMap: {get: (key: string) => key === 'brigadeCode' ? 'B1' : null}
+        } as ActivatedRouteSnapshot;
+        const tripService = TestBed.inject(TripService);
+
+        (executeResolver(route, {} as RouterStateSnapshot) as Observable<GetAllTripsResponse>)
+            .subscribe(response => {
+                expect(tripService.getTripsByRouteAndFilterLineOrName).toHaveBeenCalledWith('test-agency', 'R1');
+                expect(response).toEqual(tripsResponse);
+                expect(response.lines?.[0]?.trips?.length).toBe(2);
+                done();
+            });
     });
 });
