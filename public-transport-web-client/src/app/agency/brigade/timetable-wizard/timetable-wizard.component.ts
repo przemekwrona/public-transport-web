626928315b77f99f035ae20970b376c21ec76cd6
przemekwrona <przemekwrona25@gmail.com>
2026-09-05 17:52:10 +0200

    feat: add TimetableWizardComponent with basic structure and test setup

---
+++ public-transport-web-client/src/app/agency/brigade/timetable-wizard/timetable-wizard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-timetable-wizard',
+  imports: [],
+  templateUrl: './timetable-wizard.component.html',
+  styleUrl: './timetable-wizard.component.scss'
+})
+export class TimetableWizardComponent {
+
+}
