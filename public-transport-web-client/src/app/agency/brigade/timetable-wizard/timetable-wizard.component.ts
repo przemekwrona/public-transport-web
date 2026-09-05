3d6c77b0cfea48f892998fee425f78caa35b6728
przemekwrona <przemekwrona25@gmail.com>
2026-09-05 17:56:17 +0200

    feat: enhance TimetableWizardComponent with route handling, dynamic parameters, and updated layout

--- public-transport-web-client/src/app/agency/brigade/timetable-wizard/timetable-wizard.component.ts
+++ public-transport-web-client/src/app/agency/brigade/timetable-wizard/timetable-wizard.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, RouterModule} from '@angular/router';
 
 @Component({
-  selector: 'app-timetable-wizard',
-  imports: [],
-  templateUrl: './timetable-wizard.component.html',
-  styleUrl: './timetable-wizard.component.scss'
+    selector: 'app-timetable-wizard',
+    imports: [
+        RouterModule
+    ],
+    templateUrl: './timetable-wizard.component.html',
+    styleUrl: './timetable-wizard.component.scss'
 })
-export class TimetableWizardComponent {
+export class TimetableWizardComponent implements OnInit {
+
+    public brigadeCode: string | null = null;
+    public calendarSymbol: string | null = null;
+
+    constructor(private route: ActivatedRoute) {
+    }
+
+    ngOnInit(): void {
+        this.route.paramMap.subscribe(params => {
+            this.brigadeCode = params.get('brigadeCode');
+            this.calendarSymbol = params.get('calendarSymbol');
+        });
+    }
 
 }
