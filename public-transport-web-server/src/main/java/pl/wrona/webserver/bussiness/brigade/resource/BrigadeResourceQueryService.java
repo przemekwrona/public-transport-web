5d94191a07feb515a300f8d7f2e30b9da831232c
przemekwrona <przemekwrona25@gmail.com>
2026-09-06 03:08:27 +0200

    feat: add new query for brigade resources by group and rename methods in BrigadeResourceQueryService for clarity

--- public-transport-web-server/src/main/java/pl/wrona/webserver/bussiness/brigade/resource/BrigadeResourceQueryService.java
+++ public-transport-web-server/src/main/java/pl/wrona/webserver/bussiness/brigade/resource/BrigadeResourceQueryService.java
@@ -19,11 +19,15 @@ public class BrigadeResourceQueryService {
         return brigadeResourceQueryRepository.findAllByBrigadeGroupBrigadeGroupIdOrderByResourceSequenceAsc(brigadeGroupId);
     }
 
+    public List<BrigadeResourceEntity> findByBrigadeGroup(BrigadeGroupEntity group) {
+        return brigadeResourceQueryRepository.findByAgencyAndCalendar(group);
+    }
+
     public BrigadeResourceEntity findByBrigadeGroupAndResourceCode(BrigadeGroupEntity group, String resourceCode) {
         return brigadeResourceQueryRepository.findByAgencyAndCalendarAndSymbolAndResourceCode(group, Hex.fromHex(resourceCode));
     }
 
-    public BrigadeResourceEntity findByBrigadeGroupAndResourceCode(
+    public BrigadeResourceEntity findByAgencyAndBrigadeCodeAndCalendarCodeAndSymbolAndResourceCode(
             String agency, String brigadeItemCode, String calendarCode, String symbol, String resourceCode) {
         return brigadeResourceQueryRepository.findByAgencyAndCalendarAndSymbolAndResourceCode(
                 agency, Hex.fromHex(brigadeItemCode), Hex.fromHex(calendarCode), symbol, Hex.fromHex(resourceCode));
