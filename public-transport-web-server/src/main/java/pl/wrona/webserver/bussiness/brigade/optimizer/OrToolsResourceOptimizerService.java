e42856047e00eda8795570aaf3dbec1f0fd89968
przemekwrona <przemekwrona25@gmail.com>
2026-09-06 10:55:45 +0200

    feat: sort brigade resources by sequence in OrToolsResourceOptimizerService

--- public-transport-web-server/src/main/java/pl/wrona/webserver/bussiness/brigade/optimizer/OrToolsResourceOptimizerService.java
+++ public-transport-web-server/src/main/java/pl/wrona/webserver/bussiness/brigade/optimizer/OrToolsResourceOptimizerService.java
@@ -2,14 +2,28 @@ package pl.wrona.webserver.bussiness.brigade.optimizer;
 
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
+import org.apache.lucene.util.SloppyMath;
 import org.springframework.stereotype.Service;
 import pl.wrona.webserver.bussiness.brigade.event.BrigadeEventQueryService;
 import pl.wrona.webserver.bussiness.brigade.group.BrigadeGroupQueryService;
+import pl.wrona.webserver.bussiness.brigade.optimizer.OrToolsBrigadeOptimizer.TripRequest;
 import pl.wrona.webserver.bussiness.brigade.resource.BrigadeResourceQueryService;
+import pl.wrona.webserver.bussiness.stop.FirstAndLastStop;
 import pl.wrona.webserver.bussiness.stop.StopQueryService;
+import pl.wrona.webserver.bussiness.trip.measure.TripDistanceMeasureService;
+import pl.wrona.webserver.core.AgencyService;
+import pl.wrona.webserver.core.agency.AgencyEntity;
+import pl.wrona.webserver.core.agency.TripProfileEntity;
+import pl.wrona.webserver.core.brigade.BrigadeEventCommandRepository;
 import pl.wrona.webserver.core.brigade.BrigadeEventEntity;
 import pl.wrona.webserver.core.brigade.BrigadeResourceEntity;
 import pl.wrona.webserver.security.PreAgencyAuthorize;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 @AllArgsConstructor
@@ -28,7 +42,9 @@ public class OrToolsResourceOptimizerService {
             return;
         }
 
-        var resources = brigadeResourceQueryService.findByBrigadeGroup(brigadeGroup);
+        var resources = brigadeResourceQueryService.findByBrigadeGroup(brigadeGroup).stream()
+                .sorted(Comparator.comparing(BrigadeResourceEntity::getResourceSequence, Comparator.nullsLast(Integer::compareTo)))
+                .toList();
         var resourceIds = resources.stream()
                 .map(BrigadeResourceEntity::getBrigadeResourceId)
                 .toList();
