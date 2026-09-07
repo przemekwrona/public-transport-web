3d186352613d66868980d9e02a8555eac607dbd0
przemekwrona <przemekwrona25@gmail.com>
2026-09-07 22:29:32 +0200

    feat: update timetable resolver logic and export helper function
    - Exported `getRouteParam` for reuse across resolvers.
    - Replaced `tripsResolver` with a new `tripListTimetableResolver` to fetch route stops specifically.
    - Adjusted routes to align `timetable` resolver with updated logic.

--- public-transport-web-client/src/app/agency/trips/trip-list/trip-list-timetable/trip-list-timetable.resolver.ts
+++ public-transport-web-client/src/app/agency/trips/trip-list/trip-list-timetable/trip-list-timetable.resolver.ts
@@ -1,3 +1,17 @@
-import {tripsResolver} from "../trip-list.resolver";
+import {inject} from "@angular/core";
+import {ActivatedRouteSnapshot, ResolveFn, RouterStateSnapshot} from "@angular/router";
+import {Observable} from "rxjs";
+import {RouteService, RouteStops} from "../../../../generated/public-transport-api";
+import {LoginService} from "../../../../auth/login.service";
+import {getRouteParam} from "../trip-list.resolver";
 
-export const tripListTimetableResolver = tripsResolver;
+export const tripListTimetableResolver: ResolveFn<Observable<RouteStops>> = (
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+): Observable<RouteStops> => {
+    const authService: LoginService = inject(LoginService);
+    const routeService: RouteService = inject(RouteService);
+    const routeCode: string = getRouteParam(route, 'routeCode')!;
+
+    return routeService.getRouteStops(authService.getInstance(), routeCode);
+};
